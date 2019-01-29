3a2f3957852f4dfca9179b4b5a048a40cdf40cf0
ZsgsDesign <zsgsdesign@gmail.com>
2019-01-29 22:06:46 +0800

    blade update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,18 +5,18 @@
     <meta charset="UTF-8">
     <title>@yield('title') | @yield('site')</title>
     <!-- Copyright Information -->
-    <meta name="author" content="<{$version_info['author']}>">
-    <meta name="organization" content="<{$version_info['organization']}>">
-    <meta name="developer" content="<{$version_info['developer']}>">
-    <meta name="version" content="<{$version_info['version']}>">
-    <meta name="subversion" content="<{$version_info['subversion']}>">
+    <meta name="author" content="">
+    <meta name="organization" content="">
+    <meta name="developer" content="">
+    <meta name="version" content="">
+    <meta name="subversion" content="">
     <!-- Necessarily Declarations -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta name="format-detection" content="telephone=no">
     <meta name="renderer" content="webkit">
     <meta http-equiv="Cache-Control" content="no-siteapp" />
-    <link rel="alternate icon" type="image/png" href="<{$ATSAST_CDN}>/img/atsast/favicon.png">
+    <link rel="alternate icon" type="image/png" href="https://cdn.mundb.xyz/img/atsast/favicon.png">
     <!-- Loading Style -->
     <style>
     loading > div{
@@ -113,22 +113,57 @@
     </loading>
     <!-- Style -->
     <link rel="stylesheet" href="https://fonts.geekzu.org/css?family=Roboto:300,300i,400,400i,500,500i,700,700i">
-    <link rel="stylesheet" href="<{$ATSAST_CDN}>/css/bootstrap-material-design.min.css">
-    <link rel="stylesheet" href="<{$ATSAST_CDN}>/css/wemd-color-scheme.css">
-    <link rel="stylesheet" href="<{$ATSAST_CDN}>/css/atsast.css">
-    <link rel="stylesheet" href="<{$ATSAST_CDN}>/fonts/MDI-WXSS/MDI.css">
-    <link rel="stylesheet" href="<{$ATSAST_CDN}>/fonts/Devicon/devicon.css">
+    <link rel="stylesheet" href="https://cdn.mundb.xyz/css/bootstrap-material-design.min.css">
+    <link rel="stylesheet" href="https://cdn.mundb.xyz/css/wemd-color-scheme.css">
+    <link rel="stylesheet" href="https://cdn.mundb.xyz/css/atsast.css">
+    <link rel="stylesheet" href="https://cdn.mundb.xyz/fonts/MDI-WXSS/MDI.css">
+    <link rel="stylesheet" href="https://cdn.mundb.xyz/fonts/Devicon/devicon.css">
     <!-- Background -->
     <div class="mundb-background-container">
-        <img src="<{$bg}>">
+        <img src="">
     </div>
-    <{include file="navbar.html" }>
-    <{include file=$__template_file}>
-    <{include file="footer.html" }>
-    <script src="<{$ATSAST_CDN}>/js/jquery-3.2.1.min.js"></script>
-    <script src="<{$ATSAST_CDN}>/js/popper.min.js"></script>
-    <script src="<{$ATSAST_CDN}>/js/snackbar.min.js"></script>
-    <script src="<{$ATSAST_CDN}>/js/bootstrap-material-design.js"></script>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="margin-bottom:30px;">
+    <a class="navbar-brand" href="https://mundb.xyz/"><img src="https://cdn.mundb.xyz/img/atsast/icon_white.png" height="30"> AT SAST</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item />">
+            <a class="nav-link" href="https://mundb.xyz/">发现 <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item  active />">
+            <a class="nav-link" href="https://mundb.xyz/courses">课程</a>
+        </li>
+        <li class="nav-item />">
+            <a class="nav-link" href="https://mundb.xyz/contests">活动</a>
+        </li>
+        <li class="nav-item />">
+            <a class="nav-link" href="https://mundb.xyz/pastebin">PasteBin</a>
+        </li>
+        </ul>
+
+        <ul class="navbar-nav mundb-nav-right">
+            <form action="https://mundb.xyz/search" method="get" class="form-inline my-2 my-lg-0 mundb-inline">
+                <span class="bmd-form-group"><input class="form-control mr-sm-2 atsast-searchBox" name="q" type="search" placeholder="搜索" aria-label="搜索"></span>
+            </form>
+
+            <li class="nav-item mundb-no-shrink />">
+                <a class="nav-link" href="https://mundb.xyz/account">登录 / 注册</a>
+            </li>
+            </ul>
+    </div>
+    </nav>
+
+    @yield('template')
+
+    <footer class="mundb-footer bg-dark text-light d-print-none">
+        Copyright© Auxiliary Teaching for SAST 2018, all rights reserved.
+    </footer>
+    <script src="https://cdn.mundb.xyz/js/jquery-3.2.1.min.js"></script>
+    <script src="https://cdn.mundb.xyz/js/popper.min.js"></script>
+    <script src="https://cdn.mundb.xyz/js/snackbar.min.js"></script>
+    <script src="https://cdn.mundb.xyz/js/bootstrap-material-design.js"></script>
     <script>
         $(document).ready(function () { $('body').bootstrapMaterialDesign(); });
         window.addEventListener("load",function() {
@@ -136,13 +171,13 @@
             // compatable with proxy
 
             $.each($("a"),function(i,link){
-                $(link).attr("href",$(link).attr("href").replace(/https:\/\/static.1cf.co/gi,'<{$ATSAST_CDN}>'));
-                // link.href=link.href.replace(/https:\/\/static.1cf.co/gi,'<{$ATSAST_CDN}>'); 
+                $(link).attr("href",$(link).attr("href").replace(/https:\/\/static.1cf.co/gi,'https://cdn.mundb.xyz'));
+                // link.href=link.href.replace(/https:\/\/static.1cf.co/gi,'https://cdn.mundb.xyz');
             });
 
             $.each($("img"),function(i,image){
-                $(image).attr("src",$(image).attr("src").replace(/https:\/\/static.1cf.co/gi,'<{$ATSAST_CDN}>'));
-                // image.src=image.src.replace(/https:\/\/static.1cf.co/gi,'<{$ATSAST_CDN}>'); 
+                $(image).attr("src",$(image).attr("src").replace(/https:\/\/static.1cf.co/gi,'https://cdn.mundb.xyz'));
+                // image.src=image.src.replace(/https:\/\/static.1cf.co/gi,'https://cdn.mundb.xyz');
             });
 
             $('loading').css({"opacity":"0","pointer-events":"none"});
@@ -152,7 +187,7 @@
             var consoleSVG = "data:image/svg+xml,<svg version='1.1' id='Ebene_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' width='600px' height='100px' viewBox='0 0 600 100'> <style type='text/css'> <![CDATA[ text %7B filter: url(%23filter); fill: black; font-family: 'Share Tech Mono', consolas, sans-serif; font-size: 100px; -webkit-font-smoothing: antialiased; -moz-osx-font-smoothing: grayscale; %7D ]]> </style> <defs> <filter id='filter'> <feFlood flood-color='white' result='black' /> <feFlood flood-color='red' result='flood1' /> <feFlood flood-color='limegreen' result='flood2' /> <feOffset in='SourceGraphic' dx='3' dy='0' result='off1a'/> <feOffset in='SourceGraphic' dx='2' dy='0' result='off1b'/> <feOffset in='SourceGraphic' dx='-3' dy='0' result='off2a'/> <feOffset in='SourceGraphic' dx='-2' dy='0' result='off2b'/> <feComposite in='flood1' in2='off1a' operator='in' result='comp1' /> <feComposite in='flood2' in2='off2a' operator='in' result='comp2' /> <feMerge x='0' width='100%25' result='merge1'> <feMergeNode in = 'black' /> <feMergeNode in = 'comp1' /> <feMergeNode in = 'off1b' /> <animate attributeName='y' id = 'y' dur ='4s' values = '104px; 104px; 30px; 105px; 30px; 2px; 2px; 50px; 40px; 105px; 105px; 20px; 6%C3%9Fpx; 40px; 104px; 40px; 70px; 10px; 30px; 104px; 102px' keyTimes = '0; 0.362; 0.368; 0.421; 0.440; 0.477; 0.518; 0.564; 0.593; 0.613; 0.644; 0.693; 0.721; 0.736; 0.772; 0.818; 0.844; 0.894; 0.925; 0.939; 1' repeatCount = 'indefinite' /> <animate attributeName='height' id = 'h' dur ='4s' values = '10px; 0px; 10px; 30px; 50px; 0px; 10px; 0px; 0px; 0px; 10px; 50px; 40px; 0px; 0px; 0px; 40px; 30px; 10px; 0px; 50px' keyTimes = '0; 0.362; 0.368; 0.421; 0.440; 0.477; 0.518; 0.564; 0.593; 0.613; 0.644; 0.693; 0.721; 0.736; 0.772; 0.818; 0.844; 0.894; 0.925; 0.939; 1' repeatCount = 'indefinite' /> </feMerge> <feMerge x='0' width='100%25' y='60px' height='65px' result='merge2'> <feMergeNode in = 'black' /> <feMergeNode in = 'comp2' /> <feMergeNode in = 'off2b' /> <animate attributeName='y' id = 'y' dur ='4s' values = '103px; 104px; 69px; 53px; 42px; 104px; 78px; 89px; 96px; 100px; 67px; 50px; 96px; 66px; 88px; 42px; 13px; 100px; 100px; 104px;' keyTimes = '0; 0.055; 0.100; 0.125; 0.159; 0.182; 0.202; 0.236; 0.268; 0.326; 0.357; 0.400; 0.408; 0.461; 0.493; 0.513; 0.548; 0.577; 0.613; 1' repeatCount = 'indefinite' /> <animate attributeName='height' id = 'h' dur = '4s' values = '0px; 0px; 0px; 16px; 16px; 12px; 12px; 0px; 0px; 5px; 10px; 22px; 33px; 11px; 0px; 0px; 10px' keyTimes = '0; 0.055; 0.100; 0.125; 0.159; 0.182; 0.202; 0.236; 0.268; 0.326; 0.357; 0.400; 0.408; 0.461; 0.493; 0.513; 1' repeatCount = 'indefinite' /> </feMerge> <feMerge> <feMergeNode in='SourceGraphic' /> <feMergeNode in='merge1' /> <feMergeNode in='merge2' /> </feMerge> </filter> </defs> <g> <text x='0' y='100'>AT SAST</text> </g> </svg>";
             var consoleCSS = "background: url(\"" + consoleSVG + "\") left top no-repeat; font-size: 100px;line-height:140px;";
             console.log('%c       ', consoleCSS);
-            console.info("ATSAST - Auxiliary Teaching for SAST\n\nAuthor: <{$version_info['author']}>\nOrganization: <{$version_info['organization']}>\nDeveloper: <{$version_info['developer']}>\nVersion: <{$version_info['version']}>\nVersion Number: <{$version_info['subversion']}>\n");
+            console.info("ATSAST - Auxiliary Teaching for SAST\n");
 
         }, false);
     </script>
