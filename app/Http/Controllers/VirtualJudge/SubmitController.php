47408f82569f9321df8500ee76a1aed7e0e92af6
ZsgsDesign <zsgsdesign@gmail.com>
2019-02-12 16:00:38 +0800

    update submission

--- app/Http/Controllers/VirtualJudge/SubmitController.php
+++ app/Http/Controllers/VirtualJudge/SubmitController.php
@@ -2,21 +2,73 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Problem;
+use App\Models\Submission;
 use App\Http\Controllers\Controller;
 
 class SubmitController extends Controller
 {
+    public $ret=[];
+
     /**
-     * Show the Contest Page.
+     * Initial
      *
      * @return Response
      */
-    public function index()
+    public function __construct()
     {
-        return view('contest.index', [
-            'page_title'=>"Contest",
-            'site_title'=>"CodeMaster"
-        ]);
+        $this->ret['statue']='OK';
+        $this->ret['solution']="";
+        $this->validate_solution();
+        if($this->ret['statue']=='OK')
+        {
+            set_time_limit(0);
+
+            $sub=array(
+            'TIME'=>'0',
+            'Verdict'=>'Waiting',
+            'Soultion'=>'',
+            'Language'=>'',
+            'submission_date'=>time(),
+            'memory'=>'0',
+            'user_Handle'=>$_COOKIE['user_handle'],
+            'Problem_id'=>'',
+            );
+            $curl =new postsoutionController($sub,$_POST['oj']);
+
+            // insert submission
+
+            $submission = new Submission();
+
+
+        }
+        echo json_encode($this->ret);
+    }
+
+    /**
+     * Validate whether the solution is legal.
+     *
+     * @return Response
+     */
+    private function validate_solution()
+    {
+        if(!isset($_POST['solution']))
+        {$this->ret['statue']='NOT';return;}
+        $solution=trim($_POST['solution']);
+        if(strlen($solution)==0)
+        {
+            $this->ret['statue']='NOT';
+            $this->ret['solution']="solution must be filled";
+            return;
+        }
+        $f = fopen("cookie/file.txt", "w") or die("Unable to open file!");
+        fwrite($f,$solution);
+        fclose($f);
+        $size=filesize('cookie/file.txt');
+        if($size>100*1000)
+        {
+            $this->ret['statue']='NOT';
+            $this->ret['solution']="solution length is too big";
+            return;
+        }
     }
 }
