498716cd33cab6ffcbc6ac57ce9517893aff7c97
ZsgsDesign <zsgsdesign@gmail.com>
2019-02-14 13:50:54 +0800

    update group index

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\GroupModel;
 use App\Http\Controllers\Controller;
 
 class GroupController extends Controller
@@ -13,9 +14,14 @@ class GroupController extends Controller
      */
     public function index()
     {
+        $groupModel=new GroupModel();
+        $tending_list=$groupModel->tending_list();
+        $mine_list=$groupModel->mine_list();
         return view('group.index', [
-            'page_title'=>"Group",
-            'site_title'=>"CodeMaster"
+            'page_title' => "Group",
+            'site_title' => "CodeMaster",
+            'tending' => $tending_list,
+            'mine' => $mine_list
         ]);
     }
 
