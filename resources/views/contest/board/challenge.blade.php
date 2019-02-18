c70834852f4ee6142142a086f139641a6030cfc0
zsgsdesign <zsgsdesign@gmail.com>
2019-02-18 10:54:57 +0800

    update challenge view

--- resources/views/contest/board/challenge.blade.php
+++ resources/views/contest/board/challenge.blade.php
@@ -139,6 +139,12 @@
         <div class="col-sm-12 col-md-4">
             <paper-card>
                 <h5>Announcements</h5>
+                <div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                <div style="text-align:right;">
+                    <button class="btn btn-primary">See More</button>
+                </div>
             </paper-card>
         </div>
     </div>
