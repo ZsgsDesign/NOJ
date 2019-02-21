44bbb95de451712a8b7de97c84a268622e9510c4
zsgsdesign <zsgsdesign@gmail.com>
2019-02-21 08:51:03 +0800

    update view

--- app/Models/GroupModel.php
+++ app/Models/GroupModel.php
@@ -59,4 +59,37 @@ class GroupModel extends Model
         }
         return $user_list;
     }
+
+    public function formatPostTime($date)
+    {
+        $periods = array("second", "minute", "hour", "day", "week", "month", "year", "decade");
+        $lengths = array("60","60","24","7","4.35","12","10");
+
+        $now = time();
+        $unix_date = strtotime($date);
+
+        if (empty($unix_date)) {
+            return "Bad date";
+        }
+
+        if ($now > $unix_date) {
+            $difference = $now - $unix_date;
+            $tense = "ago";
+        } else {
+            $difference = $unix_date - $now;
+            $tense = "from now";
+        }
+
+        for ($j = 0; $difference >= $lengths[$j] && $j < count($lengths)-1; $j++) {
+            $difference /= $lengths[$j];
+        }
+
+        $difference = round($difference);
+
+        if ($difference != 1) {
+            $periods[$j].= "s";
+        }
+
+        return "$difference $periods[$j] {$tense}";
+    }
 }
