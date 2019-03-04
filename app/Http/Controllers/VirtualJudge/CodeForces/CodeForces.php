39090b106d9c6afa922c1393312f09833267bc81
ZsgsDesign <zsgsdesign@gmail.com>
2019-03-04 16:11:22 +0800

    init CodeForces Judger

---
+++ app/Http/Controllers/VirtualJudge/CodeForces/CodeForces.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Http\Controllers\VirtualJudge\CodeForces;
+
+use App\Http\Controllers\VirtualJudge\Curl;
+use App\Models\JudgerModel;
+use App\Models\ProblemModel;
+use App\Models\ResponseModel;
+use Illuminate\Support\Facades\Validator;
+
+class CodeForces extends Curl
+{
+
+}
