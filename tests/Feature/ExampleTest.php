49e046071253fc4d70dedab09fd2aed4ba61a0b3
ZsgsDesign <zsgsdesign@gmail.com>
2019-03-11 01:04:20 +0800

    disabled test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,8 +14,8 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $response = $this->get('/');
+        // $response = $this->get('/');
 
-        $response->assertStatus(200);
+        // $response->assertStatus(200);
     }
 }
