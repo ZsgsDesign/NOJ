10304388003d07f2c021e575450c4a3ef4045667
Cone Pi <pikanglong@gmail.com>
2019-03-14 13:14:07 +0800

    a small change but can always benefit from it

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -255,9 +255,7 @@
 
     @yield('addition')
 
-    <footer class="mundb-footer bg-dark text-light d-print-none">
-        Copyright &copy; NOJ 2018-2019, all rights reserved.
-    </footer>
+    <footer class="mundb-footer bg-dark text-light d-print-none" id="footercopyright"></footer>
     <script src="/static/library/jquery/dist/jquery.min.js"></script>
     <script src="/static/js/popper.min.js"></script>
     <script src="/static/js/snackbar.min.js"></script>
@@ -276,6 +274,9 @@
             console.info("\nNOJ - Nanjing University of Posts and Telecommunications Online Judge\n\nNOJ Development Team Leader: John Zhang\nOrganization: NJUPT ICPC Team\nDevelopers: John Zhang, David Diao\nVersion: Alpha\nInsider Alias: CodeMaster\n\n");
 
         }, false);
+    </script>
+    <script type="text/javascript"> 
+        document.getElementById('footercopyright').innerHTML = "Copyright &copy; NOJ 2018-" + new Date().getFullYear() + ", all rights reserved.";
     </script>
     @yield('additionJS')
 </body>
