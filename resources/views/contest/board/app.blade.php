766a5e8152a4f8f1d64344278827c505e8128de3
ZsgsDesign <zsgsdesign@gmail.com>
2019-03-15 07:29:07 +0800

    add contest notification

--- resources/views/contest/board/app.blade.php
+++ resources/views/contest/board/app.blade.php
@@ -3,5 +3,27 @@
 @section('addition')
 
     <!-- Content here would be shown at every page of the contest except IEditor -->
+    <script>
+        setInterval(()=>{
+            $.ajax({
+                type: 'POST',
+                url: '/ajax/contest/fetchClarification',
+                data: {
+                    cid: {{$cid}}
+                },
+                dataType: 'json',
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }, success: function(ret){
+                    console.log(ret);
+                    if(ret.ret==200){
+                        if(ret.data){
+                            alert(ret.data);
+                        }
+                    }
+                }
+            });
+        });
+        </script>
 
 @endsection
