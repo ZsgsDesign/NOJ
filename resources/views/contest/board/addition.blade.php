5cdd36206a5ab7c808a4e8494bf3aba463a0ca76
ZsgsDesign <zsgsdesign@gmail.com>
2019-03-18 02:26:31 +0800

    modify Contest Detail

--- resources/views/contest/board/addition.blade.php
+++ resources/views/contest/board/addition.blade.php
@@ -2,29 +2,35 @@
 
     <!-- Content here would be shown at every page of the contest -->
     <script>
-        setInterval(()=>{
-            $.ajax({
-                type: 'POST',
-                url: '/ajax/contest/fetchClarification',
-                data: {
-                    cid: {{$cid}}
-                },
-                dataType: 'json',
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                }, success: function(ret){
-                    console.log(ret);
-                    if(ret.ret==200){
-                        if(ret.data){
-                            if(localStorage.ccid!=ret.data.ccid) {
-                                alert(ret.data.content, ret.data.title, "bullhorn");
-                                localStorage.ccid=ret.data.ccid;
+        window.addEventListener("load",function() {
+            function fetchClarification() {
+                $.ajax({
+                    type: 'POST',
+                    url: '/ajax/contest/fetchClarification',
+                    data: {
+                        cid: {{$cid}}
+                    },
+                    dataType: 'json',
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    }, success: function(ret){
+                        console.log(ret);
+                        if(ret.ret==200){
+                            if(ret.data){
+                                if(localStorage.ccid!=ret.data.ccid) {
+                                    alert(ret.data.content, ret.data.title, "bullhorn");
+                                    localStorage.ccid=ret.data.ccid;
+                                }
                             }
                         }
                     }
-                }
-            });
-        }, 15000);
+                });
+            }
+            fetchClarification();
+            setInterval(()=>{
+                fetchClarification();
+            }, 15000);
+        }, false);
     </script>
 
 @endsection
