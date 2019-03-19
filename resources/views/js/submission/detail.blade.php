d3de2dd204d38c61028600a0d9539f97cbfd4d04
ZsgsDesign <zsgsdesign@gmail.com>
2019-03-20 07:06:37 +0800

    add hljs

--- resources/views/js/submission/detail.blade.php
+++ resources/views/js/submission/detail.blade.php
@@ -45,6 +45,15 @@
                         </tr>
                     </tbody>
                 </table>
+<pre>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    return 0;
+}
+</pre>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-primary" data-dismiss="modal">OK</button>
@@ -69,7 +78,56 @@
             }, success: function(ret) {
                 console.log(ret);
                 if(ret.ret==200){
-                    console.log("Show Modal");
+                    var id = new Date().getTime();
+                    $('body').append(`
+                    <div class="modal fade" id="submission${id}" data-backdrop="static" tabindex="-1" role="dialog">
+                        <div class="modal-dialog modal-dialog-centered modal-dialog-submission" role="document">
+                            <div class="modal-content sm-modal">
+                                <div class="modal-header">
+                                    <h5 class="modal-title"><i class="MDI script"></i> Submission Detail</h5>
+                                </div>
+                                <div class="modal-body">
+                                    <table class="table table-reflow">
+                                        <thead>
+                                            <tr>
+                                                <th scope="col">Status</th>
+                                                <th scope="col">Time</th>
+                                                <th scope="col">Memory</th>
+                                                <th scope="col">Lang</th>
+                                                <th scope="col">Submitted</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td class="${ret.data.color}">${ret.data.verdict}</td>
+                                                <td>${ret.data.time}ms</td>
+                                                <td>${ret.data.memory}kb</td>
+                                                <td>${ret.data.language}</td>
+                                                <td>${new Date(ret.data.submission_date * 1000).toLocaleString()}</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+<pre>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    return 0;
+}
+</pre>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-primary" data-dismiss="modal">OK</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    `);
+                    $(`#submission${id}`).on('shown.bs.modal', function (e) {
+                        changeDepth();
+                    });
+                    $(`#submission${id}`).modal('toggle');
                 } else {
                     alert(ret.desc);
                 }
