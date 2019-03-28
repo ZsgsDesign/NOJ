bd46c14f7a33b4f5c08e5fd53a2cbabc138381b9
Cone Pi <pikanglong@gmail.com>
2019-03-28 22:22:26 +0800

    Deleted confusing letters in generatepassword()

--- app/Models/AccountModel.php
+++ app/Models/AccountModel.php
@@ -10,7 +10,7 @@ class AccountModel extends Model
 {
     public function generatePassword($length=8)
     {
-        $chars='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        $chars='abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ123456789';
 
         $password='';
         for ($i=0; $i<$length; $i++) {
