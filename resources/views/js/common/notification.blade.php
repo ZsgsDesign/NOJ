b9f5dddb063d97a22d3b7be947aec4d9e1ca445e
ZsgsDesign <zsgsdesign@gmail.com>
2019-04-13 10:30:59 +0800

    add notification

---
+++ resources/views/js/common/notification.blade.php
@@ -0,0 +1,28 @@
+<script>
+function notify(title,body,icon="/static/img/njupt.png",tag="default"){
+    if (window.Notification) {
+        showMess(title,body,icon,tag);
+    } else {
+        console.log("Doesn't Support Web Notifications API");
+    }
+}
+
+function showMess(title,body,icon,tag){
+    if(window.Notification && Notification.permission !== "denied") {
+        Notification.requestPermission(function(status) {
+            if (status === "granted") {
+                var m = new Notification(title, {
+                    body: body,
+                    tag: tag,
+                    icon: icon
+                });
+                m.onclick = function () {
+                    window.focus();
+                }
+            } else{
+                console.log("Doesn't Support Web Notification Entity");
+            }
+        });
+    }
+}
+</script>
