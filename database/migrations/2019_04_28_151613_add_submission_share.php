57f722e357015749603c0f5796075820b6031a75
ZsgsDesign <zsgsdesign@gmail.com>
2019-04-28 15:19:59 +0800

    add db structure for sharing

---
+++ database/migrations/2019_04_28_151613_add_submission_share.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSubmissionShare extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('submission', function(Blueprint $table)
+        {
+            $table->boolean('share')->nullable()->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('submission', function (Blueprint $table)
+        {
+            $table->dropColumn(['share']);
+        });
+    }
+}
