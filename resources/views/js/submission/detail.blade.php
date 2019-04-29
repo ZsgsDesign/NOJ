13caff3e67ff1215c7eb20aff8688056c2491e4a
ZsgsDesign <zsgsdesign@gmail.com>
2019-04-29 12:28:42 +0800

    fixed typo

--- resources/views/js/submission/detail.blade.php
+++ resources/views/js/submission/detail.blade.php
@@ -80,14 +80,14 @@
                     $(`#submission${id}`).modal('toggle');
                     if(ret.data.solution!==null) {
                         $(`#submission${id} pre`).text(ret.data.solution);
-                        $(`#submission${id} .modal-footer button:nth-of-type(2)`).removeClass("d-none");
+                        $(`#submission${id} .modal-footer button:nth-of-type(3)`).removeClass("d-none");
                         $(`#submission${id} .modal-footer button:nth-of-type(4)`).addClass("d-none");
                         hljs.highlightBlock(document.querySelector(`#submission${id} pre`));
                     }else{
                         $(`#submission${id} pre`).remove();
                     }
                     if(ret.data.owner){
-                        $(`#submission${id} .modal-footer button:nth-of-type(3)`).removeClass("d-none");
+                        $(`#submission${id} .modal-footer button:nth-of-type(2)`).removeClass("d-none");
                     }
                 } else {
                     alert(ret.desc);
