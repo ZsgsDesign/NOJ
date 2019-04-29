a340a2df5a7926e14592e190189e92060de4a32a
ZsgsDesign <zsgsdesign@gmail.com>
2019-04-29 18:10:37 +0800

    fixed typo

--- database/migrations/2019_04_28_234338_create_pastebin_table.php
+++ database/migrations/2019_04_28_234338_create_pastebin_table.php
@@ -5,35 +5,35 @@ use Illuminate\Database\Schema\Blueprint;
 
 class CreatePastebinTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('pastebin', function(Blueprint $table)
-		{
-			$table->integer('pbid', true);
-			$table->string('lang', 50)->nullable();
-			$table->string('title')->nullable()->default('Untitled');
-			$table->integer('uid')->nullable()->index('pastebin_uid');
-			$table->dateTime('expire')->nullable();
-			$table->text('content', 16777215)->nullable();
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pastebin', function(Blueprint $table)
+        {
+            $table->integer('pbid', true);
+            $table->string('lang', 50)->nullable();
+            $table->string('title')->nullable()->default('Untitled');
+            $table->integer('uid')->nullable()->index('pastebin_uid');
+            $table->dateTime('expire')->nullable();
+            $table->text('content', 16777215)->nullable();
             $table->string('code', 10)->nullable()->unique('code');
             $table->dateTime('create_date')->nullable();
-		});
-	}
+        });
+    }
 
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('pastebin');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('pastebin');
+    }
 
 }
--- database/migrations/2019_04_28_234339_add_foreign_keys_to_pastebin_table.php
+++ database/migrations/2019_04_28_234339_add_foreign_keys_to_pastebin_table.php
@@ -5,31 +5,31 @@ use Illuminate\Database\Schema\Blueprint;
 
 class AddForeignKeysToPastebinTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::table('pastebin', function(Blueprint $table)
-		{
-			$table->foreign('uid', 'pastebin_uid')->references('id')->on('users')->onUpdate('CASCADE')->onDelete('CASCADE');
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pastebin', function(Blueprint $table)
+        {
+            $table->foreign('uid', 'pastebin_uid')->references('id')->on('users')->onUpdate('CASCADE')->onDelete('CASCADE');
+        });
+    }
 
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::table('pastebin', function(Blueprint $table)
-		{
-			$table->dropForeign('pastebin_uid');
-		});
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pastebin', function(Blueprint $table)
+        {
+            $table->dropForeign('pastebin_uid');
+        });
+    }
 
 }
