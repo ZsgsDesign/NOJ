513363486a81692ac47e6e65fe3a2f03fc6d9de9
ZsgsDesign <zsgsdesign@gmail.com>
2019-04-29 13:03:22 +0800

    add sharing for Model

--- app/Models/SubmissionModel.php
+++ app/Models/SubmissionModel.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
+use App\Models\Tool\PastebinModel;
 
 class SubmissionModel extends Model
 {
@@ -502,5 +503,41 @@ class SubmissionModel extends Model
             "paginator"=>$paginator,
             "records"=>$records
         ];
+    }
+
+    public function share($sid,$uid)
+    {
+        $basic=DB::table($this->tableName)->where(['sid'=>$sid,'uid'=>$uid])->first();
+        if(empty($basic)) return [];
+        DB::table($this->tableName)->where(['sid'=>$sid])->update([
+            "share"=>$basic["share"]?0:1
+        ]);
+        return [
+            "share"=>$basic["share"]?0:1
+        ];
+    }
+
+    public function sharePB($sid,$uid)
+    {
+        $basic=DB::table($this->tableName)->where(['sid'=>$sid,'uid'=>$uid])->first();
+        $problem=DB::table("problem")->where(['pid'=>$basic["pid"]])->first();
+        $compiler=DB::table("compiler")->where(['coid'=>$basic["coid"]])->first();
+        if(empty($basic)) return [];
+        $pastebinModel=new PastebinModel();
+        $ret=$pastebinModel->generate([
+            "syntax"=>$compiler["lang"],
+            "expiration"=>0,
+            "content"=>$basic["solution"],
+            "title"=>$problem["pcode"]." - ".$basic["verdict"],
+            "uid"=>$uid
+        ]);
+
+        if(is_null($ret)){
+            return ResponseModel::err(1001);
+        }else{
+            return ResponseModel::success(200, null, [
+                "code" => $ret
+            ]);
+        }
     }
 }
