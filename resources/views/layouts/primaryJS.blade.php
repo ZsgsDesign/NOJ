7c4847e109c6426acdb26e7baebebe0cdecc60a4
ZsgsDesign <zsgsdesign@gmail.com>
2019-05-13 06:30:23 +0800

    update with google analytics

--- resources/views/layouts/primaryJS.blade.php
+++ resources/views/layouts/primaryJS.blade.php
@@ -1,3 +1,15 @@
+@if(env("GOOGLE_ANALYTICS"))
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id={{env("GOOGLE_ANALYTICS")}}"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', '{{env("GOOGLE_ANALYTICS")}}');
+    </script>
+@endif
+
 <script>
     $(document).ready(function () { $('body').bootstrapMaterialDesign();$('[data-toggle="tooltip"]').tooltip(); });
     window.addEventListener("load",function() {
