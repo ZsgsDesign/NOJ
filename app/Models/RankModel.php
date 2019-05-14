9ea88c791964412860d32fbfa8376631ac5397af
ZsgsDesign <zsgsdesign@gmail.com>
2019-05-14 16:55:18 +0800

    update with ranking system

--- app/Models/RankModel.php
+++ app/Models/RankModel.php
@@ -6,10 +6,23 @@ use GrahamCampbell\Markdown\Facades\Markdown;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 
-class GroupModel extends Model
+class RankModel extends Model
 {
-    public $rankiing=[
-        ""
+    public $professionalRankiing=[
+        "None"=>"wemd-black-text"
+    ];
+
+    public $casualRanking=[
+        "Legendary Grandmaster"=>"cm-colorful-text",
+        "International Grandmaster"=>"wemd-pink-text",
+        "Grandmaster"=>"wemd-red-text",
+        "International Master"=>"wemd-amber-text",
+        "Master"=>"wemd-orange-text",
+        "Candidate Master"=>"wemd-purple-text",
+        "Expert"=>"wemd-blue-text",
+        "Specialist"=>"wemd-cyan-text",
+        "Pupil"=>"wemd-green-text",
+        "Newbie"=>"wemd-gray-text",
     ];
 
     public function list()
