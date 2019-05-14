d7b5e741e67fae88729311c89a2f47e2db31a818
ZsgsDesign <zsgsdesign@gmail.com>
2019-05-14 16:35:49 +0800

    add view for ranking

---
+++ resources/views/rank/index.blade.php
@@ -0,0 +1,149 @@
+@extends('layouts.app')
+
+@section('template')
+<style>
+    paper-card {
+        display: block;
+        box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 30px;
+        border-radius: 4px;
+        transition: .2s ease-out .0s;
+        color: #7a8e97;
+        background: #fff;
+        padding: 1rem;
+        position: relative;
+        border: 1px solid rgba(0, 0, 0, 0.15);
+        margin-bottom: 2rem;
+    }
+
+    paper-card:hover {
+        box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 40px;
+    }
+
+    a:hover{
+        text-decoration: none!important;
+    }
+
+    nav-div{
+        display: block;
+        margin-bottom: 0;
+        border-bottom: 2px solid rgba(0, 0, 0, 0.15);
+    }
+
+    nav-item{
+        display: inline-block;
+        color: rgba(0, 0, 0, 0.42);
+        padding: 0.25rem 0.75rem;
+        font-size: 0.85rem;
+    }
+
+    nav-item.active{
+        color: rgba(0, 0, 0, 0.93);
+        color: #03a9f4;
+        border-bottom: 2px solid #03a9f4;
+        margin-bottom: -2px;
+    }
+
+    h5{
+        margin-bottom: 1rem;
+        font-weight: bold;
+    }
+
+    .table thead th,
+    .table td,
+    .table tr{
+        vertical-align: middle;
+        text-align: center;
+        font-size:0.75rem;
+        color: rgba(0, 0, 0, 0.93);
+        transition: .2s ease-out .0s;
+    }
+
+    .table tbody tr:hover{
+        background:rgba(0,0,0,0.05);
+    }
+
+    .table thead th.cm-problem-header{
+        padding-top: 0.25rem;
+        padding-bottom: 0.05rem;
+        border:none;
+    }
+
+    .table thead th.cm-problem-subheader{
+        font-size:0.75rem;
+        padding-bottom: 0.25rem;
+        padding-top: 0.05rem;
+    }
+
+    th[scope^="row"]{
+        vertical-align: middle;
+        text-align: left;
+    }
+
+    .cm-subtext{
+        color:rgba(0, 0, 0, 0.42);
+    }
+
+    .table td.wemd-teal-text{
+        font-weight: bold;
+    }
+
+    .table td.wemd-teal-text .cm-subtext{
+        font-weight: normal;
+    }
+
+    th{
+        white-space: nowrap;
+    }
+    .cm-me{
+        background: rgba(255, 193, 7, 0.1);
+    }
+
+    .cm-shared{
+        background: rgba(76, 175, 80, 0.1);
+    }
+
+    .alert.cm-notification{
+        margin:1rem
+    }
+
+</style>
+<div class="container mundb-standard-container">
+    <paper-card>
+        <div>
+            <div class="table-responsive">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col"></th>
+                            <th scope="col">NOJ Credits</th>
+                            <th scope="col">Basis</th>
+                            <th scope="col">Community</th>
+                            <th scope="col">Practises</th>
+                            <th scope="col">Contests</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row"><img src=""></th>
+                            <td>123</td>
+                            <td>50</td>
+                            <td>100</td>
+                            <td>80</td>
+                            <td>0</td>
+                        </tr>
+                    </tbody>
+                </table>
+                {{$records["paginator"]->links()}}
+            </div>
+        </div>
+    </paper-card>
+</div>
+<script>
+
+    window.addEventListener("load",function() {
+
+    }, false);
+
+</script>
+@include('js.submission.detail')
+@endsection
