94ac56829a5ca76703093506a177977b2eb7be6f
ZsgsDesign <zsgsdesign@gmail.com>
2019-05-16 13:31:53 +0800

    init calc

---
+++ app/Models/Rating/RatingCalculator.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models\Rating;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Cache;
+use Storage;
+
+class RatingCalculator extends Model
+{
+    public function calculate($cid){
+
+    }
+}
