d64fd17c24860cf69fe6ec5f784e604792b8c63a
ZsgsDesign <zsgsdesign@gmail.com>
2019-05-16 23:05:12 +0800

    add double ranking

--- resources/views/account/dashboard.blade.php
+++ resources/views/account/dashboard.blade.php
@@ -315,7 +315,8 @@
                 </avatar-section>
                 <basic-section>
                     <h3>{{$info["name"]}}</h3>
-                    @unless(is_null($info["rankTitle"]))<small class="{{$info["professionalTitleColor"]}}">{{$info["professionalTitle"]}}</small>@endunless
+                    @unless(is_null($info["professionalTitle"]))<p class="mb-0"><small class="{{$info["professionalTitleColor"]}}">{{$info["professionalTitle"]}}</small></p>@endunless
+                    @unless(is_null($info["rankTitle"]))<p class="mb-0"><small class="{{$info["rankTitleColor"]}}">{{$info["rankTitle"]}}</small></p>@endunless
                     {{-- <p style="margin-bottom: .5rem;"><small class="wemd-light-blue-text">站点管理员</small></p> --}}
                     {{-- <p>{{$info["email"]}}</p> --}}
                 </basic-section>
