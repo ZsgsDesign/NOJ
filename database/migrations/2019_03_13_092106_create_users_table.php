b531b4fd4022a35eb51ecaa8b617dc8e37e9c9bf
ZsgsDesign <zsgsdesign@gmail.com>
2019-05-29 00:02:16 +0800

    remove unique

--- database/migrations/2019_03_13_092106_create_users_table.php
+++ database/migrations/2019_03_13_092106_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('avatar')->nullable();
-            $table->string('email')->unique();
+            $table->string('email');
             $table->timestamp('email_verified_at')->nullable();
             $table->integer('contest_account')->nullable()->index('users_contest_account');
             $table->string('password');
