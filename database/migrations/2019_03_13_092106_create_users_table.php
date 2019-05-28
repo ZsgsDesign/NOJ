7cd14a2f303afde44ee7f09aec021c2daf031c5a
ZsgsDesign <zsgsdesign@gmail.com>
2019-05-29 00:18:36 +0800

    fixed Laravel 5.7 to 5.8 bugs

--- database/migrations/2019_03_13_092106_create_users_table.php
+++ database/migrations/2019_03_13_092106_create_users_table.php
@@ -14,7 +14,7 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
             $table->string('name');
             $table->string('avatar')->nullable();
             $table->string('email');
