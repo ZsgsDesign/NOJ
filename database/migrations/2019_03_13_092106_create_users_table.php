a24b418478a6f75db18e4c5449d1fd5b04344760
ZsgsDesign <zsgsdesign@gmail.com>
2019-05-29 01:19:42 +0800

    forced InnoDB support and foregin key bugs fixed

--- database/migrations/2019_03_13_092106_create_users_table.php
+++ database/migrations/2019_03_13_092106_create_users_table.php
@@ -14,7 +14,7 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
+            $table->integer('id', true);
             $table->string('name');
             $table->string('avatar')->nullable();
             $table->string('email');
