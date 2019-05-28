d549877144e3b5109d8309ebf64636b79661da98
ZsgsDesign <zsgsdesign@gmail.com>
2019-05-28 13:06:17 +0800

    update primaryJS

--- resources/views/layouts/primaryJS.blade.php
+++ resources/views/layouts/primaryJS.blade.php
@@ -113,6 +113,6 @@
     {
         var exdate=new Date();
         exdate.setDate(exdate.getDate()+expiredays);
-        document.cookie=c_name+ "=" +escape(value)+((expiredays==null) ? "" : ";expires="+exdate.toGMTString());
+        document.cookie=c_name+ "=" +escape(value)+((expiredays==null) ? "" : ";expires="+exdate.toGMTString()) + ";domain={{env('SESSION_DOMAIN')}}";
     }
 </script>
