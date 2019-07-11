7ea0fa3dbe95ac8afc4db2304eac215ef336f0db
ChenKS <42082890+ChenKS12138@users.noreply.github.com>
2019-07-11 14:29:03 +0800

    feat: group setting

--- resources/views/group/setting.blade.php
+++ resources/views/group/setting.blade.php
@@ -85,9 +85,7 @@
 <div class="container mundb-standard-container">
     <div class="row">
     </div>
-    <div class="card my-card">
-        <div class="card-body ">
-            <h4 class="card-title"><a>Setting My Group</a></h4>
+            {{-- <h4 class="card-title"><a>Setting My Group</a></h4>
             <div class="paper-card">
                 <form class="extra-info-form md-form" id="create" action="/">
                     @csrf
@@ -99,19 +97,11 @@
                         <label for="school" class="bmd-label-floating">Group Site</label>
                         <input id="groupSite" type="text" name="gcode" class="form-control"  id="school" autocomplete="off" />
                     </div>
-                    {{-- <div class="form-group" style="display:flex;align-items:flex-end">
-                        <label for="avatar" style="color:grey">Group Avatar</label>
-                        <div class="avatar-div" id="avatar">
-                            Chose
-                            <input id="groupAvatar" name="img" class="avatar-input" type="file" accept="image/" value="">
-                        </div>
-                    </div> --}}
                     <div>
                         <avatar-section>
                             <label for="avatar" style="color:grey">Group Avatar</label>
                             <div class="avatar-div" id="avatar">
                                 Chose
-                            {{-- <input id="groupAvatar" name="img" class="avatar-input" type="file" accept="image/" value=""> --}}
                         </div>
                         </avatar-section>
                     </div>
@@ -149,8 +139,82 @@
                     </div>
                 </form>
             </div>
-            <a href="#" class="btn btn-primary" id="submit" style="margin-top:30px">Submit</a>
-        </div>
+            <a href="#" class="btn btn-primary" id="submit" style="margin-top:30px">Submit</a> --}}
+
+
+            <div id="settingModal" class="" tabindex="-1" role="dialog">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content sm-modal" style="width: 80%">
+                        <div class="modal-header">
+                            <h5 class="modal-title"><i class="MDI settings"></i> Group setting</h5>
+                        </div>
+                        <div class="modal-body">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <group-name-setting>
+                                        <div class="form-group">
+                                            <label for="group-name" class="bmd-label-floating">Group Name</label>
+                                            <input type="text" class="form-control" id="group-name" value="{{$basic_info['name']}}">
+                                        </div>
+                                        <small id="group-name-tip" class="text-center" style="display:block">PRESS ENTER TO APPLY THE CHANGES</small>
+                                    </group-name-setting><br>
+                                    <join-policy-setting style="display:block">
+                                        <p>Join Policy</p>
+                                        <div class="text-center">
+                                            <div class="btn-group">
+                                                <button id="policy-choice-btn" class="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true"
+                                                    aria-expanded="false">
+                                                    @if($basic_info['join_policy']==3)<span>Invitation & Application</span>@elseif(($basic_info['join_policy']==2))<span>Application</span>@else<span>Invitation</span>@endif
+                                                </button>
+                                                <div class="dropdown-menu text-center">
+                                                    <a class="dropdown-item join-policy-choice" data-policy="3">Invitation & Application</a>
+                                                    <a class="dropdown-item join-policy-choice" data-policy="2">Application only</a>
+                                                    <a class="dropdown-item join-policy-choice" data-policy="1">Invitation only</a>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                    </join-policy-setting>
+                                    <focus-images-setting style="display:block">
+                                        <p>Change Group Image</p>
+                                        <small id="change-image-tip" class="text-center" style="display:block">CLICK IMAGE TO CHOOSE A LOCAL IMAGE</small>
+                                        <input id="image-file" type="file" style="display:none" accept=".jpg,.png,.jpeg,.gif" />
+                                        <label for="image-file" style="display: block; cursor: pointer;" class="text-center">
+                                            <img class="group-image" style="width: 90%; height: auto;display:inline-block" src="{{$basic_info['img']}}">
+                                        </label>
+                                    </focus-images-setting>
+                                </div>
+                                <div class="col-md-6">
+                                    <permission-setting>
+                                        <p>Permission Setting</p>
+                                        @foreach($member_list as $m)
+                                            @if($m["role"]>0)
+                                            <user-card id="user-permission-{{$m["uid"]}}">
+                                                <user-avatar>
+                                                    <a href="/user/{{$m["uid"]}}"><img src="{{$m["avatar"]}}"></a>
+                                                </user-avatar>
+                                                <user-info data-clearance="{{$m["role"]}}" data-rolecolor="{{$m["role_color"]}}">
+                                                    <p><span class="badge badge-role {{$m["role_color"]}}">{{$m["role_parsed"]}}</span> <span class="cm-user-name">{{$m["name"]}}</span> @if($m["nick_name"])<span class="cm-nick-name">({{$m["nick_name"]}})</span>@endif</p>
+                                                    <p>
+                                                        <small><i class="MDI google-circles"></i> {{$m["sub_group"]}}</small>
+                                                        @if($group_clearance>$m["role"])
+                                                            <small @if($group_clearance <= $m["role"] + 1) style="display:none" @endif class="wemd-green-text cm-operation clearance-up" onclick="changeMemberClearance({{$m['uid']}},'promote')"><i class="MDI arrow-up-drop-circle-outline"></i> Promote</small>
+                                                            <small @if($m["role"] <= 1) style="display:none" @endif class="wemd-red-text cm-operation clearance-down" onclick="changeMemberClearance({{$m['uid']}},'demote')"><i class="MDI arrow-down-drop-circle-outline"></i> Demote</small>
+                                                        @endif
+                                                    </p>
+                                                </user-info>
+                                            </user-card>
+                                            @endif
+                                        @endforeach
+                                    </permission-setting>
+                                </div>
+                            </div>
+                        </div>
+                        {{-- <div class="modal-footer">
+                            <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+                        </div> --}}
+                    </div>
+                </div>
     </div>
 </div>
 <div class="modal fade" id="update-avatar-modal" tabindex="-1" role="dialog">
@@ -263,6 +327,285 @@ $('#avatar-submit').on('click',function(){
     }
     $('#update-avatar-modal').modal('hide');
 });
+
+
+
+$('.join-policy-choice').on('click',function(){
+            if($('#policy-choice-btn').text().trim() == $(this).text()) return;
+            var join_policy = $(this).text();
+            var choice = $(this).attr('data-policy');
+            $.ajax({
+                type: 'POST',
+                url: '/ajax/group/changeJoinPolicy',
+                data: {
+                    gid: {{$basic_info["gid"]}},
+                    join_policy: choice
+                },
+                dataType: 'json',
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }, success: function(result){
+                    if (result.ret===200) {
+                        changeText('#join-policy-display',{
+                            text : join_policy,
+                        });
+                        changeText('#policy-choice-btn',{
+                            text : join_policy,
+                        });
+                    } else {
+                        alert(result.desc);
+                    }
+                    ajaxing=false;
+                }, error: function(xhr, type){
+                    console.log('Ajax error while posting to joinGroup!');
+                    alert("Server Connection Error");
+                    ajaxing=false;
+                }
+            });
+        });
+
+        $('#image-file').change(function(){
+            var file = $(this).get(0).files[0];
+
+            if(file == undefined){
+                changeText('#change-image-tip',{
+                    text : 'PLEASE CHOOSE A LOCAL FILE',
+                    css : {color:'#f00'}
+                });
+                return;
+            }
+
+            if(file.size/1024 > 1024){
+                changeText('#change-image-tip',{
+                    text : 'THE SELECTED FILE IS TOO LARGE',
+                    css : {color:'#f00'}
+                });
+                return;
+            }
+
+            $(this).addClass('updating');
+            var data = new FormData();
+            data.append('img',file);
+            data.append('gid',{{$basic_info["gid"]}});
+
+            $.ajax({
+                type: 'POST',
+                url: '/ajax/group/changeGroupImage',
+                data: data,
+                processData : false,
+                contentType : false,
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }, success: function(result){
+                    if (result.ret===200) {
+                        changeText('#change-image-tip',{
+                            text : 'GROUP IMAGE CHANGE SUCESSFUL',
+                            css : {color:'#4caf50'}
+                        });
+                        $('group-image img').attr('src',result.data);
+                        $('.group-image').attr('src',result.data);
+                    } else {
+                        changeText('#change-image-tip',{
+                            text : result.desc,
+                            css : {color:'#4caf50'}
+                        });
+                    }
+                    ajaxing=false;
+                }, error: function(xhr, type){
+                    console.log('Ajax error while posting to joinGroup!');
+                    alert("Server Connection Error");
+                    ajaxing=false;
+                }
+            });
+
+            //todo call api
+
+            //read the new url from json and replace the old
+
+
+        });
+
+        $('#group-name').keydown(function(e){
+            if(e.keyCode == '13'){
+                var name = $(this).val();
+                if(name == ''){
+                    changeText('#group-name-tip',{
+                        text : 'THE NAME OF THE GROUP CANNOT BE EMPTY',
+                        css : {color:'#f00'}
+                    });
+                    return;
+                }
+                $.ajax({
+                    type: 'POST',
+                    url: '/ajax/group/changeGroupName',
+                    data: {
+                        gid: {{$basic_info["gid"]}},
+                        group_name: name
+                    },
+                    dataType: 'json',
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    }, success: function(result){
+                        if (result.ret===200) {
+                            changeText('#group-name-display',{
+                                text : name,
+                            });
+                            changeText('#group-name-tip',{
+                                text : 'GROUP NAME CHANGE SUCESSFUL',
+                                css : {color:'#4caf50'}
+                            });
+                        } else {
+                            changeText('#group-name-tip',{
+                                text : result.desc,
+                                color : '#f00',
+                            });
+                        }
+                        ajaxing=false;
+                    }, error: function(xhr, type){
+                        console.log('Ajax error while posting to joinGroup!');
+                        alert("Server Connection Error");
+                        ajaxing=false;
+                    }
+                });
+            }
+        });
+
+        $('#problemCode').bind('keypress',function(event){
+            if(event.keyCode == "13") {
+                addProblem();
+            }
+        });
+
+        $("#addProblemBtn").click(function() {
+            addProblem();
+        });
+
+        $("#joinGroup").click(function() {
+            if(ajaxing) return;
+            ajaxing=true;
+            $("#joinGroup > i").removeClass("d-none");
+            $.ajax({
+                type: 'POST',
+                url: '/ajax/joinGroup',
+                data: {
+                    gid: {{$basic_info["gid"]}}
+                },
+                dataType: 'json',
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }, success: function(result){
+                    console.log(result);
+                    if (result.ret===200) {
+                        $('#joinGroup').html('Waiting').attr('disabled','true');
+                    } else {
+                        alert(result.desc);
+                    }
+                    ajaxing=false;
+                    $("#joinGroup > i").addClass("d-none");
+                }, error: function(xhr, type){
+                    console.log('Ajax error while posting to joinGroup!');
+                    alert("Server Connection Error");
+                    ajaxing=false;
+                    $("#joinGroup > i").addClass("d-none");
+                }
+            });
+        });
+
+        $("#changeProfileBtn").click(function() {
+            if(ajaxing) return;
+            ajaxing=true;
+            $("#changeProfileBtn > i").removeClass("d-none");
+            $.ajax({
+                type: 'POST',
+                url: '/ajax/group/changeNickName',
+                data: {
+                    gid: {{$basic_info["gid"]}},
+                    nick_name: $("#nick_name").val()
+                },
+                dataType: 'json',
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }, success: function(ret){
+                    console.log(ret);
+                    if (ret.ret==200) {
+                        location.reload();
+                    } else {
+                        alert(ret.desc);
+                    }
+                    ajaxing=false;
+                    $("#changeProfileBtn > i").addClass("d-none");
+                }, error: function(xhr, type){
+                    console.log('Ajax error while posting to changeNickName!');
+                    alert("Server Connection Error");
+                    ajaxing=false;
+                    $("#changeProfileBtn > i").addClass("d-none");
+                }
+            });
+        });
+
+        $("#arrangeBtn").click(function() {
+            if(ajaxing) return;
+            else ajaxing=true;
+            var contestName = $("#contestName").val();
+            var contestBegin = $("#contestBegin").val();
+            var contestEnd = $("#contestEnd").val();
+            var problemSet = "";
+            var contestDescription = editor.getValue();
+            $("#contestProblemSet td:first-of-type").each(function(){
+                problemSet+=""+$(this).text()+",";
+            });
+            console.log(contestDescription);
+            if (contestName.replace(/(^s*)|(s*$)/g, "").length == 0) {
+                ajaxing=false;
+                return alert("Contest Name Shoudn't be empty");
+            }
+            if (contestBegin.replace(/(^s*)|(s*$)/g, "").length == 0) {
+                ajaxing=false;
+                return alert("Contest Begin Time Shoudn't be empty");
+            }
+            if (contestEnd.replace(/(^s*)|(s*$)/g, "").length == 0) {
+                ajaxing=false;
+                return alert("Contest End Time Shoudn't be empty");
+            }
+            var beginTimeParsed=new Date(Date.parse(contestBegin)).getTime();
+            var endTimeParsed=new Date(Date.parse(contestEnd)).getTime();
+            if(endTimeParsed < beginTimeParsed+60000){
+                ajaxing=false;
+                return alert("Contest length should be at least one minute.");
+            }
+            $("#arrangeBtn > i").removeClass("d-none");
+            $.ajax({
+                type: 'POST',
+                url: '/ajax/arrangeContest',
+                data: {
+                    problems: problemSet,
+                    name: contestName,
+                    description: contestDescription,
+                    begin_time: contestBegin,
+                    end_time: contestEnd,
+                    gid: {{$basic_info["gid"]}}
+                },
+                dataType: 'json',
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }, success: function(ret){
+                    console.log(ret);
+                    if (ret.ret==200) {
+                        alert(ret.desc);
+                        //location.reload();
+                    } else {
+                        alert(ret.desc);
+                    }
+                    ajaxing=false;
+                    $("#arrangeBtn > i").addClass("d-none");
+                }, error: function(xhr, type){
+                    console.log('Ajax error while posting to arrangeContest!');
+                    alert("Server Connection Error");
+                    ajaxing=false;
+                    $("#arrangeBtn > i").addClass("d-none");
+                }
+            });
+        });
 })
 </script>
 
