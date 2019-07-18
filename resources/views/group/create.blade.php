ad24e80923f62bedf8189ff5361c718e3ac54656
X3ZvaWQ <x3zvawq@gmail.com>
2019-07-18 17:47:17 +0800

    sloved some trivial questions of group create

--- resources/views/group/create.blade.php
+++ resources/views/group/create.blade.php
@@ -122,57 +122,55 @@
                 @csrf
                 <div class="row">
                     <div class="col-12 col-lg-4">
-                        <focus-image>
-                            <img id="avatar-preview" src="/static/img/group/default.png" onclick="$('#avatar-file').click();">
+                        <focus-image style="cursor: pointer">
+                            <img id="avatar-preview" src="/static/img/group/addphoto.jpg" onclick="$('#avatar-file').click();">
                             <input type="file" style="display:none" id="avatar-file" accept=".jpg,.png,.jpeg,.gif">
                         </focus-image>
                     </div>
                     <div class="col-12 col-lg-8">
                         <div class="form-group">
-                            <label for="contact" class="bmd-label-floating">Group Name</label>
-                            <input id="groupName" type="text" name="name" class="form-control" id="contact" autocomplete="off" />
+                            <label for="groupName" class="bmd-label-floating">Group Name</label>
+                            <input id="groupName" type="text" name="name" class="form-control" autocomplete="off" />
                         </div>
                         <div class="form-group">
-                            <label for="school" class="bmd-label-floating">Short Code</label>
-                            <input id="groupSite" type="text" name="gcode" class="form-control"  id="school" autocomplete="off" />
+                            <label for="groupSite" class="bmd-label-floating">Short Code</label>
+                            <input id="groupSite" type="text" name="gcode" class="form-control"  autocomplete="off" />
                         </div>
                         <div class="form-group">
-                            <label for="location" class="bmd-label-floating">Group Description</label>
-                            <input id="groupDescription" type="text" name="description" class="form-control"  id="location" autocomplete="off" />
+                            <label for="groupDescription" class="bmd-label-floating">Group Description</label>
+                            <input id="groupDescription" type="text" name="description" class="form-control"  autocomplete="off" />
                         </div>
                         <div class="form-group">
                             <label for="location" class="bmd-label-floating">Join Policy</label>
                             <div class="input-group text-center" style="display: flex;justify-content: center; align-items: center;">
                                 <div class="input-group-prepend">
-                                    <button id="gender-btn" class="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <button id="join-policy" class="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                         Default
                                     </button>
                                     <div class="dropdown-menu" style="font-size: .75rem">
-                                        <a class="dropdown-item gender-select" onclick="$('#gender-btn').text('Invite Only');$('#gender').val(1);$('#gender-input').fadeOut(200);">Invite Only</a>
-                                        <a class="dropdown-item gender-select" onclick="$('#gender-btn').text('Apply Only');$('#gender').val(2);$('#gender-input').fadeOut(200);">Apply Only</a>
-                                        <a class="dropdown-item gender-select" onclick="$('#gender-btn').text('Both');$('#gender').val(3);$('#gender-input').fadeOut(200);">Both</a>
+                                        <a class="dropdown-item gender-select" onclick="$('#join-policy').text('Invite Only');$('#policy').val(1);$('#join-policy').fadeIn(200);">Invite Only</a>
+                                        <a class="dropdown-item gender-select" onclick="$('#join-policy').text('Apply Only');$('#policy').val(2);$('#join-policy').fadeIn(200);">Apply Only</a>
+                                        <a class="dropdown-item gender-select" onclick="$('#join-policy').text('Both');$('#policy').val(3);$('#join-policy').fadeIn(200);">Both</a>
                                     </div>
                                 </div>
-                                <input style="display:none;" id="gender" name="gender" type="text" class="form-control" value="@if(!empty($extra_info['gender'])){{$extra_info['gender']}}@endif" aria-label="gender input box">
+                                <input style="display:none;" id="policy" name="policy" type="text" class="form-control" value="0" aria-label="gender input box">
                             </div>
                         </div>
                         <div class="form-group">
-                            <div class="form-group">
-                                <label for="location" class="bmd-label-floating">Is Public</label>
-                                <div class="switch">
-                                    <label>
-                                        Off
-                                        <input name="public" id="groupPublic" type="checkbox">
-                                        <span class="lever"></span> On
-                                    </label>
-                                </div>
+                            <div class="switch">
+                                <label>
+                                    <input name="public" id="groupPublic" type="checkbox">Public
+                                </label>
                             </div>
                         </div>
                     </div>
                 </div>
             </form>
         </card-body>
-        <a href="#" class="btn btn-primary" id="submit" style="margin-top:30px">Submit</a>
+        <div class="text-right">
+            <button type="button" id="submit" class="btn btn-danger" style="margin-top:30px">Create</button>
+        </div>
+
     </paper-card>
 </div>
 
@@ -180,85 +178,72 @@
 <script>
 window.addEventListener('load',function(){
     document.querySelector('#submit').addEventListener('click',() => {
-    const name = document.querySelector('#groupName').value;
-    const gcode = document.querySelector('#groupSite').value;
-    const img = document.querySelector('#avatar-file').files[0];
-    const Public = document.querySelector('#groupPublic').checked === true ? 1 : 2;
-    const description = document.querySelector("#groupDescription").value;
-    const joinPolicy = document.querySelector("#gender").value;
-    const data = new FormData();
-    console.log(name,gcode,Public,description,joinPolicy);
-    data.append('name',name);
-    data.append('gcode',gcode);
-    data.append('img',img);
-    data.append('public',Public);
-    data.append('description',description);
-    data.append('join_policy',joinPolicy);
-    $.ajax({
-        url:"/ajax/group/createGroup",
-        method: 'POST',
-        data: data,
-        contentType: false,
-        processData: false,
-        cache: false,
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        }, success: function(data) {
-            alert(data.desc,'New Group');
-            location.reload();
-        },
-        error: function (jqXHR) {
-            alert(jqXHR.responseJSON.message,"New Group");
+        const name = document.querySelector('#groupName').value;
+        const gcode = document.querySelector('#groupSite').value;
+        const img = document.querySelector('#avatar-file').files[0];
+        const Public = document.querySelector('#groupPublic').checked === true ? 1 : 2;
+        const description = document.querySelector("#groupDescription").value;
+        const joinPolicy = document.querySelector("#policy").value;
+        const data = new FormData();
+        if(name.length < 3 || name.length > 50 || gcode.length < 3 || gcode.length > 50 || description > 100){
+            alert(`
+            The length of the name and short code should be less than 50 and greater than 3 <br />
+            The description length should be less than 100
+            `);
+            return;
         }
+
+        if(img == undefined){
+            alert('Please select a image');
+            return;
+        }
+
+        if(img.size/1024 > 1024){
+            $('#tip-text').text('The selected img id too large');
+            return;
+        }
+        data.append('name',name);
+        data.append('gcode',gcode);
+        data.append('img',img);
+        data.append('public',Public);
+        data.append('description',description);
+        data.append('join_policy',joinPolicy);
+        $.ajax({
+            url:"/ajax/group/createGroup",
+            method: 'POST',
+            data: data,
+            contentType: false,
+            processData: false,
+            cache: false,
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }, success: function(data) {
+                if(data.ret = 200){
+                    window.location = '/group/' + gcode;
+                }else{
+                    alert(data.desc,'New Group');
+                }
+            },
+            error: function (jqXHR) {
+                alert(jqXHR.responseJSON.message,"New Group");
+            }
+        })
     })
-})
 
 
-$('#avatar').on('click',function(){
-    $('#update-avatar-modal').modal();
-});
+    $('#avatar').on('click',function(){
+        $('#update-avatar-modal').modal();
+    });
 
-$('#avatar-file').on('change',function(){
-    var file = $(this).get(0).files[0];
+    $('#avatar-file').on('change',function(){
+        var file = $(this).get(0).files[0];
 
-    var reader = new FileReader();
-    reader.onload = function(e){
-        $('#avatar-preview').attr('src',e.target.result);
-    };
-    reader.readAsDataURL(file);
-});
-
-$('#avatar-submit').on('click',function(){
-    if($(this).is('.updating')){
-        $('#tip-text').text('SLOW DOWN');
-        $('#tip-text').addClass('text-danger');
-        $('#tip-text').removeClass('text-success');
-        $('#avatar-error-tip').animate({opacity:'1'},200);
-        return ;
-    }
-
-    var file = $('#avatar-file').get(0).files[0];
-    if(file == undefined){
-        $('#tip-text').text('PLEASE CHOOSE A LOCAL FILE');
-        $('#tip-text').addClass('text-danger');
-        $('#tip-text').removeClass('text-success');
-        $('#avatar-error-tip').animate({opacity:'1'},200);
-        return;
-    }else{
-        $('#avatar-error-tip').css({opacity:'0'});
-    }
-
-    if(file.size/1024 > 1024){
-        $('#tip-text').text('THE SELECTED FILE IS TOO LARGE');
-        $('#tip-text').addClass('text-danger');
-        $('#tip-text').removeClass('text-success');
-        $('#avatar-error-tip').animate({opacity:'1'},200);
-        return;
-    }else{
-        $('#avatar-error-tip').css({opacity:'0'});
-    }
-    $('#update-avatar-modal').modal('hide');
-});
+        var reader = new FileReader();
+        reader.onload = function(e){
+            $('#avatar-preview').attr('src',e.target.result);
+        };
+        reader.readAsDataURL(file);
+    });
 })
 </script>
 
