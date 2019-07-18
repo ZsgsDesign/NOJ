6926308d28c86ff64207a2e644456479c0df4f11
Cone Pi <pikanglong@gmail.com>
2019-07-19 01:53:03 +0800

    Modify Foreign Keys of Problem & Judger Table

---
+++ database/migrations/2019_07_19_015032_modify_foreign_keys_of_judger_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ModifyForeignKeysOfJudgerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('judger', function (Blueprint $table) {
+            $table->foreign('oid', 'judger_oid')->references('oid')->on('oj')->onUpdate('CASCADE')->onDelete('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('judger', function (Blueprint $table) {
+            $table->foreign('oid', 'judger_oid')->references('oid')->on('oj')->onUpdate('CASCADE')->onDelete('RESTRICT');
+        });
+    }
+}
