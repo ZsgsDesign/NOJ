ea10de883a5bfff6dc749667b21f2b69a9610c9d
ZsgsDesign <zsgsdesign@gmail.com>
2019-07-20 12:18:00 +0800

    modify babel path generate

--- app/Babel/Submit/Curl.php
+++ app/Babel/Submit/Curl.php
@@ -27,7 +27,7 @@ class Curl
         $datapost=curl_init();
         $headers=array("Expect:");
 
-        curl_setopt($datapost, CURLOPT_CAINFO, dirname(__FILE__)."/../Cookies/cacert.pem");
+        curl_setopt($datapost, CURLOPT_CAINFO, babel_path("Cookies/cacert.pem"));
         curl_setopt($datapost, CURLOPT_URL, $url);
         curl_setopt($datapost, CURLOPT_HEADER, true); //
         curl_setopt($datapost, CURLOPT_HTTPHEADER, $headers); //
@@ -38,8 +38,8 @@ class Curl
         curl_setopt($datapost, CURLOPT_FOLLOWLOCATION, true);
 
         curl_setopt($datapost, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($datapost, CURLOPT_COOKIEFILE, dirname(__FILE__)."/../Cookies/{$oj}_{$handle}.cookie");
-        curl_setopt($datapost, CURLOPT_COOKIEJAR, dirname(__FILE__)."/../Cookies/{$oj}_{$handle}.cookie");
+        curl_setopt($datapost, CURLOPT_COOKIEFILE, babel_path("Cookies/{$oj}_{$handle}.cookie"));
+        curl_setopt($datapost, CURLOPT_COOKIEJAR, babel_path("Cookies/{$oj}_{$handle}.cookie"));
         ob_start();
         $response=curl_exec($datapost);
         if (curl_errno($datapost)) {
@@ -59,13 +59,13 @@ class Curl
         if(isset($all_data["handle"]))  $handle = $all_data["handle"];      else $handle = "default";
 
         $ch=curl_init();
-        curl_setopt($ch, CURLOPT_CAINFO, dirname(__FILE__)."/../Cookies/cacert.pem");
+        curl_setopt($ch, CURLOPT_CAINFO, babel_path("Cookies/cacert.pem"));
         // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         // curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36");
-        curl_setopt($ch, CURLOPT_COOKIEFILE, dirname(__FILE__)."/../Cookies/{$oj}_{$handle}.cookie");
-        curl_setopt($ch, CURLOPT_COOKIEJAR, dirname(__FILE__)."/../Cookies/{$oj}_{$handle}.cookie");
+        curl_setopt($ch, CURLOPT_COOKIEFILE, babel_path("Cookies/{$oj}_{$handle}.cookie"));
+        curl_setopt($ch, CURLOPT_COOKIEJAR, babel_path("Cookies/{$oj}_{$handle}.cookie"));
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($ch, CURLOPT_URL, $site);
         ob_start();
@@ -96,7 +96,7 @@ class Curl
             $data=$data ? json_encode($data) : '{}';
             array_push($headers, 'Content-Type: application/json', 'Content-Length: '.strlen($data));
         }
-        curl_setopt($datapost, CURLOPT_CAINFO, dirname(__FILE__)."/../Cookies/cacert.pem");
+        curl_setopt($datapost, CURLOPT_CAINFO, babel_path("Cookies/cacert.pem"));
         curl_setopt($datapost, CURLOPT_URL, $site);
         curl_setopt($datapost, CURLOPT_HEADER, $returnHeader);
         curl_setopt($datapost, CURLOPT_HTTPHEADER, array_merge($headers, $extraHeaders));
@@ -107,8 +107,8 @@ class Curl
         curl_setopt($datapost, CURLOPT_FOLLOWLOCATION, $follow);
 
         curl_setopt($datapost, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($datapost, CURLOPT_COOKIEFILE, dirname(__FILE__)."/../Cookies/{$oj}_{$handle}.cookie");
-        curl_setopt($datapost, CURLOPT_COOKIEJAR, dirname(__FILE__)."/../Cookies/{$oj}_{$handle}.cookie");
+        curl_setopt($datapost, CURLOPT_COOKIEFILE, babel_path("Cookies/{$oj}_{$handle}.cookie"));
+        curl_setopt($datapost, CURLOPT_COOKIEJAR, babel_path("Cookies/{$oj}_{$handle}.cookie"));
         ob_start();
         $response=curl_exec($datapost);
         if (curl_errno($datapost)) {
