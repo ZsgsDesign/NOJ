758e9915cffad60391ae33f672faeeecc93fe58f
ZsgsDesign <zsgsdesign@gmail.com>
2019-07-26 11:34:46 +0800

    add structure

---
+++ app/Models/Search/ProblemSearchModel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Search;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class ProblemSearchModel extends Model
+{
+    protected $table='problem';
+    protected $primaryKey='pid';
+}
