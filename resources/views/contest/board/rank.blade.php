0e61cd50666736fe514e4a49b2f8a5210a2afa12
ZsgsDesign <zsgsdesign@gmail.com>
2019-07-29 11:18:41 +0800

    using css to display rank

--- resources/views/contest/board/rank.blade.php
+++ resources/views/contest/board/rank.blade.php
@@ -117,6 +117,10 @@
         height: calc(36px + 1.5rem);
     }
 
+    tbody{counter-reset: count 0;}
+    tbody > tr{counter-increment: count 1;}
+    tbody th::before{content:counter(count);}
+
 </style>
 <div class="container mundb-standard-container">
     <paper-card>
@@ -177,7 +181,7 @@
                             {{-- ACM/ICPC Mode --}}
                             @foreach($contest_rank as $r)
                             <tr class="@if($r["uid"]==Auth::user()->id) cm-me @endif">
-                                <th scope="row">{{$loop->iteration}}</th>
+                                <th scope="row">{{--$loop->iteration--}}</th>
                                 <td>{{$r["name"]}} @if($r["nick_name"])<span class="cm-subtext">({{$r["nick_name"]}})</span>@endif</td>
                                 <td>{{$r["score"]}}</td>
                                 <td>{{round($r["penalty"])}}</td>
@@ -194,7 +198,7 @@
                             {{-- OI Mode --}}
                             @foreach($contest_rank as $r)
                             <tr class="@if($r["uid"]==Auth::user()->id) cm-me @endif">
-                                <th scope="row">{{$loop->iteration}}</th>
+                                <th scope="row">{{--$loop->iteration--}}</th>
                                 <td>{{$r["name"]}} @if($r["nick_name"])<span class="cm-subtext">({{$r["nick_name"]}})</span>@endif</td>
                                 <td>{{round($r["score"])}}</td>
                                 <td>{{$r["solved"]}}</td>
