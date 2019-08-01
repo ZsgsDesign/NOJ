6d3958f04289667376144f953eb876d2bca29cf9
ZsgsDesign <zsgsdesign@gmail.com>
2019-08-01 11:04:22 +0800

    add unit test

--- tests/Feature/MainAccess.php
+++ tests/Feature/MainAccess.php
@@ -5,7 +5,7 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ExampleTest extends TestCase
+class MainAccess extends TestCase
 {
     /**
      * A basic test example.
