8b762caf625a0947e45f26a7efb25aba7ee43564
ZsgsDesign <zsgsdesign@gmail.com>
2019-08-03 11:10:10 +0800

    add NOJ Statics

--- app/Admin/Controllers/DashboardController.php
+++ app/Admin/Controllers/DashboardController.php
@@ -11,12 +11,12 @@ class DashboardController
     {
         $status = [
             ['name' => 'NOJ Version',       'value' => version()],
-            ['name' => 'Problems',       'value' => \App\Models\Eloquent\ProblemModel::count()],
-            ['name' => 'Solutions',       'value' => \App\Models\Eloquent\ProblemSolutionModel::count()],
+            ['name' => 'Problems',          'value' => \App\Models\Eloquent\ProblemModel::count()],
+            ['name' => 'Solutions',         'value' => \App\Models\Eloquent\ProblemSolutionModel::count()],
             ['name' => 'Submissions',       'value' => \App\Models\Eloquent\SubmissionModel::count()],
-            ['name' => 'Contests',       'value' => \App\Models\Eloquent\ContestModel::count()],
-            ['name' => 'Users',       'value' => \App\Models\Eloquent\UserModel::count()],
-            ['name' => 'Groups',       'value' => \App\Models\Eloquent\GroupModel::count()],
+            ['name' => 'Contests',          'value' => \App\Models\Eloquent\ContestModel::count()],
+            ['name' => 'Users',             'value' => \App\Models\Eloquent\UserModel::count()],
+            ['name' => 'Groups',            'value' => \App\Models\Eloquent\GroupModel::count()],
         ];
 
         return view('admin::dashboard.general', compact('status'));
