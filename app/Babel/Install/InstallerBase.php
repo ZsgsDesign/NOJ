6688efc6f91543348a693eef2df06266b17f340b
ZsgsDesign <zsgsdesign@gmail.com>
2019-08-05 01:35:43 +0800

    extracted _install method

--- app/Babel/Install/InstallerBase.php
+++ app/Babel/Install/InstallerBase.php
@@ -9,153 +9,40 @@ use PharIo\Version\VersionConstraintParser;
 use PharIo\Version\InvalidVersionException;
 use Illuminate\Support\Facades\DB;
 use Exception;
+use Throwable;
 
 class InstallerBase
 {
     protected $command;
     protected $versionParser;
     protected $oid=0;
+    protected $babelConfig=[];
+    protected $ocode=null;
 
     protected function _install($ocode)
     {
-        $this->command->line("Installing <fg=green>$ocode</>");
-
-        $babelConfig=json_decode(file_get_contents(babel_path("Extension/$ocode/babel.json")), true);
-
-        // support __cur__ variables
-        if ($babelConfig["version"]=="__cur__") {
-            $babelConfig["version"]=explode("-", version())[0];
-        }
-        if ($babelConfig["require"]["NOJ"]=="__cur__") {
-            $babelConfig["require"]["NOJ"]=explode("-", version())[0];
-        }
-
-        // check version info
-        if (!isset($babelConfig["version"]) || is_null($babelConfig["version"]) || trim($babelConfig["version"])=="") {
-            $this->command->line("\n  <bg=red;fg=white> Lack version info, aborting. </>\n");
-            return;
-        }
-
-        // check require info
-        if (!isset($babelConfig["require"]["NOJ"]) || is_null($babelConfig["require"]["NOJ"]) || trim($babelConfig["require"]["NOJ"])=="") {
-            $this->command->line("\n  <bg=red;fg=white> Lack NOJ compability info, aborting. </>\n");
-            return;
-        }
-
-        // check requirement
         try {
-            if (!($this->versionParser->parse($babelConfig["require"]["NOJ"])->complies(new Version(explode("-", version())[0])))) {
-                if (!$this->command->option('ignore-platform-reqs')) {
-                    $this->command->line("Your Current NOJ Version <fg=yellow>".version()."</> doesn't support the following extension: ");
-                    $this->command->line("  - <fg=green>$ocode</> requires NOJ version <fg=yellow>{$babelConfig['require']['NOJ']}</>");
-                    return;
-                }
-            }
-        } catch (InvalidVersionException $e) {
-            $this->command->line("\n  <bg=red;fg=white> Illegal Compability Info, aborting. </>\n");
-            return;
-        }
+            $this->ocode=$ocode;
+            $this->command->line("Installing <fg=green>$ocode</>");
+            $this->babelConfig=json_decode(file_get_contents(babel_path("Extension/$ocode/babel.json")), true);
 
-        DB::beginTransaction();
+            // check version info
+            $this->checkVersionInfo();
 
-        // get current installed version info
-        $info=OJModel::basic(OJModel::oid($ocode));
+            // support __cur__ variables
+            $this->parseVersion();
 
-        // if there isn't, create one
-        if (empty($info)) {
-            $this->oid=OJModel::insertOJ([
-                "ocode"=>$babelConfig["code"],
-                "name"=>$babelConfig["name"],
-                "home_page"=>$babelConfig["website"],
-                "logo"=>"/static/img/oj/default.png",
-                "status"=>1,
-                "version"=>"",
-                "compiler_timestamp"=>"",
-            ]);
-        } else {
-            // check legal version format
-            try {
-                $currentVersion=new Version($babelConfig["version"]);
-            } catch (InvalidVersionException $e) {
-                DB::rollback();
-                $this->command->line("\n  <bg=red;fg=white> Illegal Version Info, aborting. </>\n");
-                return;
-            }
+            // check requirement
+            $this->checkRequirement();
 
-            // check there is a not null version
-            if (isset($info["version"]) && !is_null($info["version"]) && trim($info["version"])!="") {
-                try {
-                    $installedVersion=new Version($info["version"]);
-                } catch (InvalidVersionException $e) {
-                    DB::rollback();
-                    $this->command->line("\n  <bg=red;fg=white> Illegal Version Info, aborting. </>\n");
-                    return;
-                }
+            //writing database
+            $this->transactionDB();
 
-                if (!($currentVersion->isGreaterThan($installedVersion))) {
-                    // lower version or even
-                    DB::rollback();
-                    $this->command->line("Nothing to install or update");
-                    return;
-                }
-            }
-
-            $this->oid=$info["oid"];
-        }
-
-        // retrieve compiler config and then import it
-        $installed_timestamp=0;
-        if (isset($info["compiler_timestamp"]) && !is_null($info["compiler_timestamp"]) && trim($info["compiler_timestamp"])!="") {
-            $installed_timestamp=intval($info["compiler_timestamp"]);
-        }
-        $latest_timestamp=$installed_timestamp;
-        $ConpilerConfig = glob(babel_path("Extension/$ocode/compiler/*.*"));
-        foreach ($ConpilerConfig as $file) {
-            if (intval(basename($file)) > $installed_timestamp) {
-                try {
-                    $this->commitCompiler($file, json_decode(file_get_contents($file), true));
-                    $latest_timestamp=intval(basename($file));
-                } catch (Exception $e) {
-                    DB::rollback();
-                    $this->command->line("<fg=red>Error:     ".$e->getMessage()."</>");
-                    $this->command->line("\n  <bg=red;fg=white> Compiler info import failure, aborting. </>\n");
-                    return;
-                }
+        }catch(Throwable $e){
+            if ($e->getMessage()!==""){
+                $this->command->line("\n  <bg=red;fg=white> {$e->getMessage()} </>\n");
             }
         }
-
-        // import css
-        try {
-            if (isset($babelConfig["custom"]["css"]) && !is_null($babelConfig["custom"]["css"]) && trim($babelConfig["custom"]["css"])!="") {
-                $cssPath=babel_path("Extension/$ocode/".$babelConfig["custom"]["css"]);
-            } else {
-                $cssPath=null;
-            }
-            $this->applyCustom($ocode, $cssPath);
-        } catch (Exception $e) {
-            DB::rollback();
-            $this->command->line("\n  <bg=red;fg=white> Unable to add an custom css for this extension, aborting. </>\n");
-            return;
-        }
-
-
-        // import icon
-        try {
-            $imgPath=babel_path("Extension/$ocode/".$babelConfig["icon"]);
-            $storePath=$this->applyIcon($ocode, $imgPath);
-            OJModel::updateOJ($this->oid, ["logo"=>$storePath]);
-        } catch (Exception $e) {
-            DB::rollback();
-            $this->command->line("\n  <bg=red;fg=white> Unable to add an icon for this extension, aborting. </>\n");
-            return;
-        }
-
-        $this->oid=OJModel::updateOJ(OJModel::oid($babelConfig["code"]), [
-            "version"=>$babelConfig["version"],
-            "compiler_timestamp"=>$latest_timestamp,
-        ]);
-
-        DB::commit();
     }
 
     protected function _uninstall($ocode)
@@ -262,5 +149,178 @@ class InstallerBase
             }
             closedir($handle);
         }
+    }
+
+    private function parseVersion()
+    {
+        if(empty($this->babelConfig)){
+            throw new Exception('Missing babel.json Config file.');
+        }
+
+        if ($this->babelConfig["version"]=="__cur__") {
+            $this->babelConfig["version"]=explode("-", version())[0];
+        }
+
+        if ($this->babelConfig["require"]["NOJ"]=="__cur__") {
+            $this->babelConfig["require"]["NOJ"]=explode("-", version())[0];
+        }
+    }
+
+    private function checkVersionInfo()
+    {
+        // check version info
+        if (!isset($this->babelConfig["version"]) || is_null($this->babelConfig["version"]) || trim($this->babelConfig["version"])=="") {
+            throw new Exception('Lack version info.');
+        }
+
+        // check require info
+        if (!isset($this->babelConfig["require"]["NOJ"]) || is_null($this->babelConfig["require"]["NOJ"]) || trim($this->babelConfig["require"]["NOJ"])=="") {
+            throw new Exception('Lack NOJ compability info.');
+        }
+    }
+
+    private function checkRequirement()
+    {
+        try {
+            if (!($this->versionParser->parse($this->babelConfig["require"]["NOJ"])->complies(new Version(explode("-", version())[0])))) {
+                if (!$this->command->option('ignore-platform-reqs')) {
+                    $this->command->line("Your Current NOJ Version <fg=yellow>".version()."</> doesn't support the following extension: ");
+                    $this->command->line("  - <fg=green>{$this->ocode}</> requires NOJ version <fg=yellow>{$this->babelConfig['require']['NOJ']}</>");
+                    throw new Exception();
+                }
+            }
+        } catch (InvalidVersionException $e) {
+            throw new Exception('Illegal Compability Info.');
+        }
+    }
+
+    private function transactionDB()
+    {
+        DB::beginTransaction();
+        $ocode=$this->ocode;
+        try {
+            // get current installed version info
+            $info=OJModel::basic(OJModel::oid($ocode));
+
+            $this->insertWhenNotExist($info);
+
+            // retrieve compiler config and then import it
+            $latest_timestamp=$this->importCompilerInfo($info);
+
+            // import css
+            $this->importCSS();
+
+            // import icon
+            $this->importICON();
+
+            $this->oid=OJModel::updateOJ(OJModel::oid($this->babelConfig["code"]), [
+                "version"=>$this->babelConfig["version"],
+                "compiler_timestamp"=>$latest_timestamp,
+            ]);
+
+            DB::commit();
+
+        }catch(Throwable $e){
+            DB::rollback();
+            if ($e->getMessage()!=="") {
+                $this->command->line("\n  <bg=red;fg=white> {$e->getMessage()} </>\n");
+            }
+            return;
+        }
+    }
+
+    private function insertWhenNotExist($info)
+    {
+        $ocode=$this->ocode;
+
+        // if there isn't, create one
+        if (empty($info)) {
+            $this->oid=OJModel::insertOJ([
+                "ocode"=>$this->babelConfig["code"],
+                "name"=>$this->babelConfig["name"],
+                "home_page"=>$this->babelConfig["website"],
+                "logo"=>"/static/img/oj/default.png",
+                "status"=>1,
+                "version"=>"",
+                "compiler_timestamp"=>"",
+            ]);
+        } else {
+            // check legal version format
+            try {
+                $currentVersion=new Version($this->babelConfig["version"]);
+            } catch (InvalidVersionException $e) {
+                throw new Exception('Illegal Version Info, aborting.');
+            }
+
+            // check there is a not null version
+            if (isset($info["version"]) && !is_null($info["version"]) && trim($info["version"])!="") {
+                try {
+                    $installedVersion=new Version($info["version"]);
+                } catch (InvalidVersionException $e) {
+                    throw new Exception('Illegal Version Info, aborting.');
+                }
+
+                if (!($currentVersion->isGreaterThan($installedVersion))) {
+                    // lower version or even
+                    $this->command->line("Nothing to install or update.");
+                    throw new Exception();
+                }
+            }
+
+            $this->oid=$info["oid"];
+        }
+    }
+
+    private function importCSS()
+    {
+        $ocode=$this->ocode;
+
+        try {
+            if (isset($this->babelConfig["custom"]["css"]) && !is_null($this->babelConfig["custom"]["css"]) && trim($this->babelConfig["custom"]["css"])!="") {
+                $cssPath=babel_path("Extension/$ocode/".$this->babelConfig["custom"]["css"]);
+            } else {
+                $cssPath=null;
+            }
+            $this->applyCustom($ocode, $cssPath);
+        } catch (Throwable $e) {
+            throw new Exception('Unable to add an custom css for this extension, aborting.');
+        }
+    }
+
+    private function importICON()
+    {
+        $ocode=$this->ocode;
+
+        try {
+            $imgPath=babel_path("Extension/$ocode/".$this->babelConfig["icon"]);
+            $storePath=$this->applyIcon($ocode, $imgPath);
+            OJModel::updateOJ($this->oid, ["logo"=>$storePath]);
+        } catch (Throwable $e) {
+            throw new Exception('Unable to add an icon for this extension, aborting.');
+        }
+    }
+
+    private function importCompilerInfo($info)
+    {
+        $ocode=$this->ocode;
+        $installed_timestamp=0;
+        if (isset($info["compiler_timestamp"]) && !is_null($info["compiler_timestamp"]) && trim($info["compiler_timestamp"])!="") {
+            $installed_timestamp=intval($info["compiler_timestamp"]);
+        }
+        $latest_timestamp=$installed_timestamp;
+        $ConpilerConfig = glob(babel_path("Extension/$ocode/compiler/*.*"));
+        foreach ($ConpilerConfig as $file) {
+            if (intval(basename($file)) > $installed_timestamp) {
+                try {
+                    $this->commitCompiler($file, json_decode(file_get_contents($file), true));
+                    $latest_timestamp=intval(basename($file));
+                } catch (Throwable $e) {
+                    $this->command->line("<fg=red>Error:     ".$e->getMessage()."</>");
+                    $this->command->line("\n  <bg=red;fg=white> Compiler info import failure, aborting. </>\n");
+                    throw new Exception();
+                }
+            }
+        }
+        return $latest_timestamp;
     }
 }
