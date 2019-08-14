694c899861854f2a3443f3591c986e038885117f
Cone Pi <pikanglong@gmail.com>
2019-08-14 18:57:49 +0800

    Hide Problem

--- app/Models/ProblemModel.php
+++ app/Models/ProblemModel.php
@@ -276,7 +276,7 @@ class ProblemModel extends Model
     {
         // $prob_list = DB::table($this->table)->select("pid","pcode","title")->get()->all(); // return a array
         $submissionModel=new SubmissionModel();
-        $preQuery=DB::table($this->table);
+        $preQuery=DB::table($this->table)->where('hide','=',0);
         if ($filter['oj']) {
             $preQuery=$preQuery->where(["OJ"=>$filter['oj']]);
         }
@@ -487,5 +487,10 @@ class ProblemModel extends Model
         }
 
         return $pid;
+    }
+
+    public function isHidden($pid)
+    {
+        return DB::table('problem')->where('pid','=',$pid)->get()->first()['hide'];
     }
 }
