b8e99a30e0eab209fc593a0e16d2651211ef1e13
ZsgsDesign <zsgsdesign@gmail.com>
2019-09-01 23:32:52 +0800

    add nav for contest panel

--- resources/views/contest/board/print.blade.php
+++ resources/views/contest/board/print.blade.php
@@ -25,26 +25,6 @@
         text-decoration: none!important;
     }
 
-    nav-div{
-        display: block;
-        margin-bottom: 0;
-        border-bottom: 2px solid rgba(0, 0, 0, 0.15);
-    }
-
-    nav-item{
-        display: inline-block;
-        color: rgba(0, 0, 0, 0.42);
-        padding: 0.25rem 0.75rem;
-        font-size: 0.85rem;
-    }
-
-    nav-item.active{
-        color: rgba(0, 0, 0, 0.93);
-        color: #03a9f4;
-        border-bottom: 2px solid #03a9f4;
-        margin-bottom: -2px;
-    }
-
     h5{
         margin-bottom: 1rem;
         font-weight: bold;
@@ -54,19 +34,7 @@
 <div class="container mundb-standard-container">
     <paper-card>
         <h5>{{$contest_name}}</h5>
-        <nav-div>
-            <a href="/contest/{{$cid}}/board/challenge"><nav-item>Challenge</nav-item></a>
-            <a href="/contest/{{$cid}}/board/rank"><nav-item>Rank</nav-item></a>
-            <a href="/contest/{{$cid}}/board/status"><nav-item>Status</nav-item></a>
-            <a href="/contest/{{$cid}}/board/clarification"><nav-item>Clarification</nav-item></a>
-            <a href="/contest/{{$cid}}/board/print"><nav-item class="active">Print</nav-item></a>
-            @if($basic['practice'])
-                <a href="/contest/{{$cid}}/board/analysis"><nav-item>Analysis</nav-item></a>
-            @endif
-            @if($clearance>2)
-            <a href="/contest/{{$cid}}/board/admin"><nav-item>Admin</nav-item></a>
-            @endif
-        </nav-div>
+        @include('contest.board.nav',['nav'=>'print'])
         <div class="p-3">
             <p style="text-align: center;">Not Supported</p>
         </div>
