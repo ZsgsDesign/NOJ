c61159f18aab7a3e8599d6de56cd0ed27f5f3703
ZsgsDesign <zsgsdesign@gmail.com>
2019-09-03 02:18:52 +0800

    agreement checked

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,6 +52,7 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:16', 'unique:users'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'agreement' => ['required'],
         ]);
     }
 
