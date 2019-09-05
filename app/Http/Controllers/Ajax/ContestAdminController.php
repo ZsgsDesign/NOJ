d4d6f25eb8aa0d8278d289d355a85c97b5e54dd8
ZsgsDesign <zsgsdesign@gmail.com>
2019-09-06 04:05:57 +0800

    remove comment

--- app/Http/Controllers/Ajax/ContestAdminController.php
+++ app/Http/Controllers/Ajax/ContestAdminController.php
@@ -326,26 +326,7 @@ class ContestAdminController extends Controller
                 'shortName'=>$record->name,
                 'date'=>date("F j, Y", strtotime($record->begin_time)),
             ],
-            'problemset'=>EloquentContestModel::getProblemSet($cid),/*[
-                [
-                    'index'=>'A',
-                    'title'=>'A+B Problem',
-                    'memory_limit'=>262144,
-                    'time_limit'=>1000,
-                    'parsed'=>[
-                        'description'=>'<p>New Year is coming in Line World! In this world, there are <span class="tex-span"><i>n</i></span> cells numbered by integers from <span class="tex-span">1</span> to <span class="tex-span"><i>n</i></span>, as a <span class="tex-span">1 × <i>n</i></span> board. People live in cells. However, it was hard to move between distinct cells, because of the difficulty of escaping the cell. People wanted to meet people who live in other cells.</p><p>So, user tncks0121 has made a transportation system to move between these cells, to celebrate the New Year. First, he thought of <span class="tex-span"><i>n</i> - 1</span> positive integers <span class="tex-span"><i>a</i><sub class="lower-index">1</sub>, <i>a</i><sub class="lower-index">2</sub>, ..., <i>a</i><sub class="lower-index"><i>n</i> - 1</sub></span>. For every integer <span class="tex-span"><i>i</i></span> where <span class="tex-span">1 ≤ <i>i</i> ≤ <i>n</i> - 1</span> the condition <span class="tex-span">1 ≤ <i>a</i><sub class="lower-index"><i>i</i></sub> ≤ <i>n</i> - <i>i</i></span> holds. Next, he made <span class="tex-span"><i>n</i> - 1</span> portals, numbered by integers from 1 to <span class="tex-span"><i>n</i> - 1</span>. The <span class="tex-span"><i>i</i></span>-th (<span class="tex-span">1 ≤ <i>i</i> ≤ <i>n</i> - 1</span>) portal connects cell <span class="tex-span"><i>i</i></span> and cell <span class="tex-span">(<i>i</i> + <i>a</i><sub class="lower-index"><i>i</i></sub>)</span>, and one can travel from cell <span class="tex-span"><i>i</i></span> to cell <span class="tex-span">(<i>i</i> + <i>a</i><sub class="lower-index"><i>i</i></sub>)</span> using the <span class="tex-span"><i>i</i></span>-th portal. Unfortunately, one cannot use the portal backwards, which means one cannot move from cell <span class="tex-span">(<i>i</i> + <i>a</i><sub class="lower-index"><i>i</i></sub>)</span> to cell <span class="tex-span"><i>i</i></span> using the <span class="tex-span"><i>i</i></span>-th portal. It is easy to see that because of condition <span class="tex-span">1 ≤ <i>a</i><sub class="lower-index"><i>i</i></sub> ≤ <i>n</i> - <i>i</i></span> one can\'t leave the Line World using portals.</p><p>Currently, I am standing at cell <span class="tex-span">1</span>, and I want to go to cell <span class="tex-span"><i>t</i></span>. However, I don\'t know whether it is possible to go there. Please determine whether I can go to cell <span class="tex-span"><i>t</i></span> by only using the construted transportation system.</p>',
-                        'input'=>"222",
-                        'output'=>"333",
-                        'note'=>"444"
-                    ],
-                    'testcases'=>[
-                        [
-                            'input'=>'1 2',
-                            'output'=>'3'
-                        ]
-                    ]
-                ]
-            ]*/
+            'problemset'=>EloquentContestModel::getProblemSet($cid),
         ])->save(storage_path("app/contest/pdf/$cid.pdf"));
 
         $record->pdf=1;
