0d983f312e3777af703e27e445dc5264a3b74818
ZsgsDesign <zsgsdesign@gmail.com>
2019-09-13 21:10:40 +0800

    add manage:create-admin

---
+++ app/Console/Commands/Manage/CreateAdminCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands\Manage;
+
+use Illuminate\Console\Command;
+
+class CreateAdminCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'manage:create-admin';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a admin user';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $userModel = config('admin.database.users_model');
+        $username = $this->ask('Please enter a username to login');
+        $password = bcrypt($this->secret('Please enter a password to login'));
+        $name = $this->ask('Please enter a name to display');
+        $user = new $userModel(compact('username', 'password', 'name'));
+        $user->save();
+        $this->info("Admin User [$name] created successfully.");
+    }
+}
