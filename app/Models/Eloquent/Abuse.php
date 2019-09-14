de9c8e6bdb1a0adbc43570592f11c91fb4250162
ZsgsDesign <zsgsdesign@gmail.com>
2019-09-14 10:03:43 +0800

    add user to abuse

--- app/Models/Eloquent/Abuse.php
+++ app/Models/Eloquent/Abuse.php
@@ -3,13 +3,24 @@
 namespace App\Models\Eloquent;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\DB;
 
 class Abuse extends Model
 {
     use SoftDeletes;
 
+    public static $supportCategory=['group'];
+    public static $cause=[
+        '0'=>'General'
+    ];
+
     protected $fillable=[
         'title', 'cause', 'supplement', 'link', 'user_id'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\Eloquent\UserModel', 'user_id');
+    }
 }
