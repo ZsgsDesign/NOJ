2e0bcd5c4eec8b976c616eda3e527ee9f99447fb
ZsgsDesign <zsgsdesign@gmail.com>
2019-09-23 12:38:01 +0800

    add DojoPass Model

---
+++ app/Models/Eloquent/Dojo/DojoPass.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models\Eloquent\Dojo;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Auth;
+
+class DojoPass extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable=[
+        'dojo_id', 'user_id'
+    ];
+
+    public function dojo()
+    {
+        return $this->belongsTo('App\Models\Eloquent\Dojo\Dojo', 'dojo_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\Eloquent\UserModel', 'user_id');
+    }
+
+    public static function isPassed($dojo_id)
+    {
+        return Auth::check()?self::where([
+            "dojo_id"=>$dojo_id,
+            "user_id"=>Auth::user()->id,
+        ])->count()>0:false;
+    }
+}
