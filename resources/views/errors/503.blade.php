a449af4dbb49c6554de8faf759f65528a26cf275
ZsgsDesign <zsgsdesign@gmail.com>
2019-10-10 19:23:54 +0800

    add message for down

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -2,7 +2,7 @@
     'emoji'=>';-&#41;',
     'code'=>503,
     'type'=>'Maintenance Mode',
-    'description'=>'NOJ is now updating or maintaining',
+    'description'=>$exception->getMessage() ?: 'NOJ is now updating or maintaining',
     'tips'=>'Please visit later'
 ])
 
