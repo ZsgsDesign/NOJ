0c40c941b94559989b424a9183776716b68a3932
X3ZvaWQ <x3zvawq@gmail.com>
2020-01-17 13:28:08 +0800

    modify: componentization dashboard.

---
+++ resources/views/account/components/abuseButton.blade.php
@@ -0,0 +1,44 @@
+<style>
+    shadow-button.btn-group{
+        position: absolute;
+        top: .5rem;
+        right: 1.5rem;
+        z-index: 2;
+        margin: 0;
+    }
+    shadow-button .btn::after{
+        display: none;
+    }
+    shadow-button .btn{
+        color:#fff!important;
+        border-radius: 100%!important;
+        padding: .5rem!important;
+        line-height: 1!important;
+        font-size: 1.5rem!important;
+    }
+    shadow-button .dropdown-item > i {
+        display: inline-block;
+        transform: scale(1.5);
+        padding-right: 0.5rem;
+        color: rgba(0,0,0,0.42);
+    }
+
+    shadow-button.btn-group .dropdown-menu {
+        border-radius: .125rem;
+    }
+
+    shadow-button .dropdown-item {
+        flex-wrap: nowrap!important;
+    }
+</style>
+<shadow-button class="btn-group">
+    <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <i class="MDI dots-vertical"></i>
+    </button>
+    <div class="dropdown-menu">
+        <button class="dropdown-item wemd-red-text" onclick="reportAbuse()"><i class="MDI alert-circle wemd-red-text"></i> Report Abuse</button>
+    </div>
+</shadow-button>
+<script>
+
+</script>
---
+++ resources/views/account/components/avatarUpdate.blade.php
@@ -0,0 +1,112 @@
+<div class="modal fade" id="update-avatar-modal" tabindex="-1" role="dialog">
+    <div class="modal-dialog modal-dialog-centered modal-dialog-alert" role="document">
+        <div class="modal-content sm-modal">
+            <div class="modal-header">
+                <h5 class="modal-title">Update your avatar</h5>
+            </div>
+            <div class="modal-body">
+                <div class="container-fluid text-center">
+                    <avatar-section>
+                        <img id="avatar-preview" src="{{$info["avatar"]}}" alt="avatar">
+                    </avatar-section>
+                    <br />
+                    <input type="file" style="display:none" id="avatar-file" accept=".jpg,.png,.jpeg,.gif">
+                    <label for="avatar-file" id="choose-avatar" class="btn btn-primary" role="button"><i class="MDI upload"></i> select local file</label>
+                </div>
+                <div id="avatar-error-tip" style="opacity:0" class="text-center">
+                    <small id="tip-text" class="text-danger font-weight-bold">PLEASE CHOOSE A LOCAL FILE</small>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button id="avatar-submit" type="button" class="btn btn-danger">Update</button>
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+    window.addEventListener("load",function() {
+        $('#avatar').on('click',function(){
+            $('#update-avatar-modal').modal();
+        });
+
+        $('#avatar-file').on('change',function(){
+            var file = $(this).get(0).files[0];
+
+            var reader = new FileReader();
+            reader.onload = function(e){
+                $('#avatar-preview').attr('src',e.target.result);
+            };
+            reader.readAsDataURL(file);
+        });
+
+        $('#avatar-submit').on('click',function(){
+            if($(this).is('.updating')){
+                $('#tip-text').text('SLOW DOWN');
+                $('#tip-text').addClass('text-danger');
+                $('#tip-text').removeClass('text-success');
+                $('#avatar-error-tip').animate({opacity:'1'},200);
+                return ;
+            }
+
+            var file = $('#avatar-file').get(0).files[0];
+            if(file == undefined){
+                $('#tip-text').text('PLEASE CHOOSE A LOCAL FILE');
+                $('#tip-text').addClass('text-danger');
+                $('#tip-text').removeClass('text-success');
+                $('#avatar-error-tip').animate({opacity:'1'},200);
+                return;
+            }else{
+                $('#avatar-error-tip').css({opacity:'0'});
+            }
+
+            if(file.size/1024 > 1024){
+                $('#tip-text').text('THE SELECTED FILE IS TOO LARGE');
+                $('#tip-text').addClass('text-danger');
+                $('#tip-text').removeClass('text-success');
+                $('#avatar-error-tip').animate({opacity:'1'},200);
+                return;
+            }else{
+                $('#avatar-error-tip').css({opacity:'0'});
+            }
+
+            $(this).addClass('updating');
+            var avatar_data = new FormData();
+            avatar_data.append('avatar',file);
+
+            $.ajax({
+                url : '{{route("account_update_avatar")}}',
+                type : 'POST',
+                data : avatar_data,
+                processData : false,
+                contentType : false,
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                },
+                success : function(result){
+                    if(result.ret == 200){
+                        $('#tip-text').text('AVATAR CHANGE SUCESSFUL');
+                        $('#tip-text').removeClass('text-danger');
+                        $('#tip-text').addClass('text-success');
+                        $('#avatar-error-tip').animate({opacity:'1'},200);
+                        var newURL = result.data;
+                        $('#avatar').attr('src',newURL);
+                        $('#atsast_nav_avatar').attr('src',newURL);
+                        setTimeout(function(){
+                            $('#update-avatar-modal').modal('hide');
+                            $('#avatar-error-tip').css({opacity:'0'});
+                            $('#avatar-submit').removeClass('updating');
+                        },1000);
+                    }else{
+                        $('#tip-text').text(result.desc);
+                        $('#tip-text').addClass('text-danger');
+                        $('#tip-text').removeClass('text-success');
+                        $('#avatar-error-tip').animate({opacity:'1'},200);
+                        setTimeout(function(){
+                            $('#avatar-submit').removeClass('updating');
+                        },1000);
+                    }
+                }
+            });
+        });
+    });
+</script>
