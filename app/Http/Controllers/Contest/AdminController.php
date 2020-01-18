aaa81985f9d5fc0cf5a703db493186f10c57d249
John Zhang <zsgsdesign@gmail.com>
2020-01-18 15:50:40 +0800

    Add package import

--- app/Http/Controllers/Contest/AdminController.php
+++ app/Http/Controllers/Contest/AdminController.php
@@ -9,6 +9,8 @@ use App\Exports\AccountExport;
 use Imtigger\LaravelJobStatus\JobStatus;
 use Auth;
 use Redirect;
+use App\Exports\AccountExport;
+use App\Models\Eloquent\ContestModel as EloquentContestModel;
 use Excel;
 use Cache;
 use DB;
