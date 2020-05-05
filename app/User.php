53a3219bc174bb562a1f2710d24868ce47a8eafe
X3ZvaWQ <x3zvawq@gmail.com>
2020-05-06 03:54:25 +0800

    new: update laravel/framework to ^6.0

--- app/User.php
+++ app/User.php
@@ -5,10 +5,11 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasApiTokens, Notifiable;
 
     /**
      * The attributes that are mass assignable.
