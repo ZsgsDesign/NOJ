025c86dd9a8ca409c6fbb9189e499e66450922b7
ZsgsDesign <zsgsdesign@gmail.com>
2020-05-07 14:07:26 +0800

    remove Nanjing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -322,7 +322,7 @@
 
                     <div class="col-md-4">
                         <h5 class="cm-footer-title mb-4 mt-3 font-bold">{{config("app.name")}}</h5>
-                        <p>{{config("app.name")}} is an online judge developed by Fangtang Zhixing Network Technology(Nanjing) Co,Ltd together with the ICPC Team of NJUPT.</p>
+                        <p>{{config("app.name")}} is an online judge developed by Fangtang Zhixing Network Technology together with the ICPC Team of NJUPT.</p>
                     </div>
 
                     <hr class="clearfix w-100 d-md-none">
