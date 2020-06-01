83e6044cb48af8316835c74b6beb1e8e5c27f59d
ZsgsDesign <zsgsdesign@gmail.com>
2020-06-01 17:09:30 +0800

    add login and register locale

---
+++ resources/lang/en/account.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    "slogan"         => ":name is yet another Online Judge",
+    "forgetpassword" => "Forget your password?",
+    "agree"          => "I agree with the",
+    "terms"          => "Terms",
+];
