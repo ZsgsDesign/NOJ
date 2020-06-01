777937808a70e4dfeb6e41df0122a142aefaac92
ZsgsDesign <zsgsdesign@gmail.com>
2020-06-01 16:26:04 +0800

    add group locale

---
+++ resources/lang/en/group.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'my'         => 'My Groups',
+    'trending'   => 'Trending Groups',
+    'members'    => '{0} :count Members|{1} :count Member|[2,*] :count Members',
+    "create"     => [
+        "title" => "Create a New Group",
+        "description" => "Click here to create your own group!",
+    ]
+];
---
+++ resources/lang/zh-CN/group.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'my'         => '我的群组',
+    'trending'   => '时下流行',
+    'members'    => '{0} :count 位成员|{1} :count 位成员|[2,*] :count 位成员',
+    "create"     => [
+        "title" => "创建新的群组",
+        "description" => "点击这里创建您的群组！",
+    ]
+];
