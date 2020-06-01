c2bf8540602871bc59d3c3be8c61a132eab3e814
ZsgsDesign <zsgsdesign@gmail.com>
2020-06-01 14:47:27 +0800

    add helper functions locale

---
+++ resources/lang/zh-CN/helper.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'time' => [
+        "after" => "前",
+        "before" => "后",
+        "singular" => [
+            "second" => "秒",
+            "minute" => "分钟",
+            "hour" => "小时",
+            "day" => "天",
+            "week" => "周",
+            "month" => "月",
+            "year" => "年",
+            "decade" => "世代"
+        ],
+        "plural" => [
+            "second" => "秒",
+            "minute" => "分钟",
+            "hour" => "小时",
+            "day" => "天",
+            "week" => "周",
+            "month" => "月",
+            "year" => "年",
+            "decade" => "世代"
+        ],
+        "formatter" => ":time:unit:tense",
+        "malformatter" => "未知时间",
+    ],
+];
