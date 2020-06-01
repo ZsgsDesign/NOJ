42292ac765094517f5c05eccba7e4ff58e17300b
ZsgsDesign <zsgsdesign@gmail.com>
2020-06-01 14:48:20 +0800

    add problem index locale

---
+++ resources/lang/en/problem.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    "problem" => "Problem",
+    "submitted" => "Submitted",
+    "passed" => "Passed",
+    "acrate" => "AC Rate",
+    "filter" => "Filter",
+    "empty" => "Nothing matches your search.",
+];
---
+++ resources/lang/zh-CN/problem.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    "problem" => "题目",
+    "submitted" => "提交",
+    "passed" => "通过",
+    "acrate" => "通过率",
+    "filter" => "筛选器",
+    "empty" => "未找到相关题目。",
+];
