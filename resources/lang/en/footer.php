d6bbf794d6075b8def54ddcad3d0411dabf295c8
ZsgsDesign <zsgsdesign@gmail.com>
2020-06-01 11:49:21 +0800

    update footer locale

--- resources/lang/en/footer.php
+++ resources/lang/en/footer.php
@@ -2,10 +2,10 @@
 
 return [
     'description'   => ':name is an online judge developed by Fangtang Zhixing Network Technology together with the ICPC Team of NJUPT.',
-    'services'   => 'Services',
-    'developments'   => 'Developments',
-    'supports'   => 'Supports',
-    'queue' => 'Judging Queue',
-    'opensource' => 'Open Source',
-    'api' => 'API',
+    'services'      => 'Services',
+    'developments'  => 'Developments',
+    'supports'      => 'Supports',
+    'queue'         => 'Judging Queue',
+    'opensource'    => 'Open Source',
+    'api'           => 'API',
 ];
