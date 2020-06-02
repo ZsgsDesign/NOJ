7a683416d38be54b41a27bd9a7d260f6ae935b93
X3ZvaWQ <x3zvawq@gmail.com>
2020-06-02 18:39:43 +0800

    add: message localization

--- resources/lang/en/group.php
+++ resources/lang/en/group.php
@@ -7,5 +7,19 @@ return [
     "create"     => [
         "title" => "Create a New Group",
         "description" => "Click here to create your own group!",
+    ],
+    'message'    => [
+        'agreeJoin' => [
+            'title'   => 'Some users agreed to join your group :name',
+            'content' => ''
+        ],
+        'appliedJoin'=> [
+            'title'   => 'Some users applied to join your group :name',
+            'content' => ''
+        ],
+        'inviteJoin' => [
+            'title'   => ':sender_name invites you to group :group_name',
+            'content' => 'Hi, Dear **:reciver_name**,\n\n  **:sender_name** has just invited you to join the group **[:group_name](:group_url)**. Take a look and meet other fascinating people right now!\n\nSincerely, NOJ'
+        ]
     ]
 ];
--- resources/lang/zh-CN/group.php
+++ resources/lang/zh-CN/group.php
@@ -7,5 +7,19 @@ return [
     "create"     => [
         "title" => "创建新的群组",
         "description" => "点击这里创建您的群组！",
+    ],
+    'message'    => [
+        'agreeJoin' => [
+            'title'   => '他们已同意加入你的群组 :name',
+            'content' => ''
+        ],
+        'appliedJoin'=> [
+            'title'   => '有人申请加入您的群组 :name',
+            'content' => ''
+        ],
+        'inviteJoin' => [
+            'title'   => ':sender_name 邀请您加入 :group_name',
+            'content' => '嗨,  **:reciver_name**,\n\n  **:sender_name** 刚刚邀请你加入 **[:group_name](:group_url)** 群组. 现在就看一看，认识一些有趣的人吧!\n\n NOJ'
+        ]
     ]
 ];
