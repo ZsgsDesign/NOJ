473d2b82e6546fffbb0915e712772b093fc0b7c1
X3ZvaWQ <x3zvawq@gmail.com>
2020-06-03 18:19:05 +0800

    add: pastebin localization

--- resources/views/tool/pastebin/view.blade.php
+++ resources/views/tool/pastebin/view.blade.php
@@ -97,7 +97,7 @@
         <a href="/user/{{$detail["userInfo"]["id"]}}"><img src="{{$detail["userInfo"]["avatar"]}}" class="cm-avatar-square"></a>
         <p>{{$detail["userInfo"]["name"]}}</p>
     </user-section>
-    <small style="color:rgba(0,0,0,0.42);">Created at {{$detail["created_at"]}}@if($detail["expire"]), Expired at {{$detail["expire"]}}@endif</small>
+    <small style="color:rgba(0,0,0,0.42);">{{__('pastebin.created_at', ['created_at' => $detail["created_at"]])}}@if($detail["expire"]), {{__('pastebin.expired_at', ['expired_at' => $detail["expire"]])}}@endif</small>
     <code-paper>
         <code-header>{{$detail["title"]}}</code-header>
         <pre data-lang="{{$detail["lang"]}}" id="pb_content">{{$detail["content"]}}</pre>
