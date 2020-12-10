d6cc69e681400eb4efdf7b13ce381fa397c26046
zsgsdesign <zsgsdesign@gmail.com>
2020-12-11 00:21:42 +0800

    remove: Devicon font

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -167,7 +167,7 @@
     <link rel="stylesheet" href="/static/css/main.css?version={{version()}}">
     <link rel="stylesheet" href="/static/library/animate.css/animate.min.css">
     <link rel="stylesheet" href="/static/fonts/mdi-wxss/MDI.css">
-    <link rel="stylesheet" href="/static/fonts/Devicon/devicon.css">
+    <link rel="stylesheet" href="/static/fonts/devicon/devicon.css">
     <!-- Background -->
     <div class="mundb-background-container">
         <img src="">
