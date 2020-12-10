8065c9ff56b57edabcef5937bd6bf7b92ffda0bd
zsgsdesign <zsgsdesign@gmail.com>
2020-12-11 01:13:31 +0800

    fixed: typo

--- resources/lang/zh-CN/errors.php
+++ resources/lang/zh-CN/errors.php
@@ -29,7 +29,7 @@ return [
         '503' => [
             'type'  => 'Maintenance Mode',
             'description'  => ':name 当前正在升级或维护中',
-            'tips'  => '请稍后再行访问'
+            'tips'  => '请稍候再行访问'
         ]
     ]
 ];
