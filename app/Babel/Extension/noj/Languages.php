dace4d1be028a56905320df37b601a399b0534c4
zsgsdesign <zsgsdesign@gmail.com>
2021-03-23 15:52:38 +0800

    add haskell language config

--- app/Babel/Extension/noj/Languages.php
+++ app/Babel/Extension/noj/Languages.php
@@ -178,6 +178,22 @@ class Languages
                     'memory_limit_check_only' => 1
                 ]
             ],
+            'haskell_lang_config' => [
+                'compile' => [
+                    'src_name' => 'main.hs',
+                    'exe_name' => 'main',
+                    'max_cpu_time' => 3000,
+                    'max_real_time' => 10000,
+                    'max_memory' => 1024 * 1024 * 1024,
+                    'compile_command' => '/usr/bin/ghc -O -outputdir /tmp -o {exe_path} {src_path}'
+                ],
+                'run' => [
+                    'command' => '{exe_path}',
+                    'seccomp_rule' => "general",
+                    'env' => $default_env,
+                    'memory_limit_check_only' => 1
+                ]
+            ],
         ];
     }
 }
