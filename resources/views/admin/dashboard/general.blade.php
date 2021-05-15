bb42fb625024485675eaac7b23c9c74c22d9bedd
aaliyahzhao <aaliyahzhao@tencent.com>
2021-05-15 12:50:40 +0800

    admin-HomeController-本地化

--- resources/views/admin/dashboard/general.blade.php
+++ resources/views/admin/dashboard/general.blade.php
@@ -28,7 +28,7 @@ version-badge > inline-div:last-of-type{
 </style>
 <div class="box box-default">
     <div class="box-header with-border">
-        <h3 class="box-title">General</h3>
+        <h3 class="box-title">{{__('admin.home_infos.dashboard')}}</h3>
 
         <div class="box-tools pull-right">
             <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
