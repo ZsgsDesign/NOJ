0501c7515c78ba9cb5ca05602611c94e4c3260e9
zsgsdesign <zsgsdesign@gmail.com>
2021-08-05 04:18:46 +0800

    remove unnecessary params

--- app/Models/Eloquent/UserBanned.php
+++ app/Models/Eloquent/UserBanned.php
@@ -16,6 +16,6 @@ class UserBanned extends Model
     }
 
     public function user() {
-        return $this->belongsTo('App\Models\Eloquent\User', 'user_id', 'id');
+        return $this->belongsTo('App\Models\Eloquent\User');
     }
 }
