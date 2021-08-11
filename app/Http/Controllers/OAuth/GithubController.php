34a83243411e262a849f3c3dff22fa5c765b8886
zsgsdesign <zsgsdesign@gmail.com>
2021-08-11 18:17:31 +0800

    complete auto register for OAuth

--- app/Http/Controllers/OAuth/GithubController.php
+++ app/Http/Controllers/OAuth/GithubController.php
@@ -6,37 +6,41 @@ use App\Http\Controllers\Controller;
 use App\Models\Eloquent\User;
 use App\Models\Eloquent\UserExtra;
 use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Database\QueryException;
 use Auth;
+use Str;
 
 class GithubController extends Controller
 {
     public function redirectTo()
     {
+        // 2 ways to access this page, you want to bind(logined), you want to login(not logined)
         if(Auth::check()){
-            if(Auth::check() && Auth::user()->hasEmailPassAccess()){
+            // logined user, only users with non-temp email & pass access and binded can unbind
+            if(!Auth::user()->isIndependent()){
                 return redirect()->route('account.settings');
             }
-        }
-        if(Auth::check() && Auth::user()->getExtra('github_id')){
-            return view('oauth.index',[
-                'page_title'=>"OAuth",
-                'site_title'=>config("app.name"),
-                'navigation'=>"OAuth",
-                'platform' => 'Github',
-                'display_html' => 'You\'re already tied to the github account : <span class="text-info">'.Auth::user()->getExtra('github_email').'</span><br />
-                You can choose to unbind or go back to the homepage',
-                'buttons' => [
-                    [
-                        'text' => 'unbind',
-                        'href' => route('oauth.github.unbind'),
-                        'style' => 'btn-danger'
-                    ],
-                    [
-                        'text' => 'home',
-                        'href' => route('home'),
-                    ],
-                ]
-            ]);
+            if(Auth::user()->getExtra('github_id')){
+                return view('oauth.index',[
+                    'page_title'=>"OAuth",
+                    'site_title'=>config("app.name"),
+                    'navigation'=>"OAuth",
+                    'platform' => 'Github',
+                    'display_html' => 'You\'re already tied to the github account : <span class="text-info">'.Auth::user()->getExtra('github_email').'</span><br />
+                    You can choose to unbind or go back to the homepage',
+                    'buttons' => [
+                        [
+                            'text' => 'unbind',
+                            'href' => route('oauth.github.unbind'),
+                            'style' => 'btn-danger'
+                        ],
+                        [
+                            'text' => 'home',
+                            'href' => route('home'),
+                        ],
+                    ]
+                ]);
+            }
         }
         return Socialite::driver('github')->redirect();
     }
@@ -98,22 +102,53 @@ class GithubController extends Controller
                 Auth::user()->setExtra('github_token', $github_user->token,101);
                 return redirect('/');
             }else{
+                if(config('app.allow_oauth_temp_account')){
+                    try {
+                        $createdUser=User::create([
+                            'name' => Str::random(12),
+                            'email' => Str::random(16)."@temporary.email",
+                            'password' => '',
+                            'avatar' => '/static/img/avatar/default.png',
+                        ]);
+                    }catch(QueryException $exception){
+                        return view('oauth.index',[
+                            'page_title'=>"OAuth",
+                            'site_title'=>config("app.name"),
+                            'navigation'=>"OAuth",
+                            'platform' => 'Github',
+                            'display_text' => 'Some wired things happened when registering your account, please contact site admin or simply retry again.',
+                            'buttons' => [
+                                [
+                                    'text' => 'retry login',
+                                    'href' => route('login'),
+                                ]
+                            ]
+                        ]);
+                    }
+                    Auth::loginUsingId($createdUser->id);
+                    Auth::user()->setExtra('github_email', $github_user->email);
+                    Auth::user()->setExtra('github_nickname', $github_user->nickname);
+                    Auth::user()->setExtra('github_homepage', ($github_user->user)['html_url']);
+                    Auth::user()->setExtra('github_token', $github_user->token, 101);
+                    return redirect('/');
+                }
+                $buttons=[[
+                    'text' => 'login',
+                    'href' => route('login'),
+                ]];
+                if(config('function.register')){
+                    $buttons[]=[
+                        'text' => 'register',
+                        'href' => route('register'),
+                    ];
+                }
                 return view('oauth.index',[
                     'page_title'=>"OAuth",
                     'site_title'=>config("app.name"),
                     'navigation'=>"OAuth",
                     'platform' => 'Github',
-                    'display_text' => 'This github account doesn\'t seem to have a '.config("app.name").' account, please register or log in first',
-                    'buttons' => [
-                        [
-                            'text' => 'login',
-                            'href' => route('login'),
-                        ],
-                        [
-                            'text' => 'register',
-                            'href' => route('register'),
-                        ],
-                    ]
+                    'display_text' => 'This github account doesn\'t seem to have a '.config("app.name").' account, please have your account binded at first place.',
+                    'buttons' => $buttons
                 ]);
             }
         }
