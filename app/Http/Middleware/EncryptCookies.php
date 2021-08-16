75cd0339100f53c7e89b10734de7561efba3dc0a
zsgsdesign <zsgsdesign@gmail.com>
2021-08-16 16:07:15 +0800

    fixed cookie value null bugs

--- app/Http/Middleware/EncryptCookies.php
+++ app/Http/Middleware/EncryptCookies.php
@@ -12,6 +12,7 @@ class EncryptCookies extends Middleware
      * @var array
      */
     protected $except=[
-        'isEmailVerifiedNoticed'
+        'isEmailVerifiedNoticed',
+        'isNotified',
     ];
 }
