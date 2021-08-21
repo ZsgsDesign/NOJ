614a3df1fc7ce6cbf726c33b1c2d70962cd26c12
zsgsdesign <zsgsdesign@gmail.com>
2021-08-21 10:11:37 +0800

    update mix config

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,29 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+// mix.js('resources/js/app.js', 'public/js')
+//    .sass('resources/sass/app.scss', 'public/css');
+
+mix.scripts([
+    'public/js/admin.js',
+    'public/js/dashboard.js'
+], 'public/static/js/noj.js');
+
+mix.styles([
+    'public/static/fonts/roboto/roboto.css',
+    'public/static/fonts/roboto/roboto.css',
+    'public/static/fonts/montserrat/montserrat.css',
+    'public/static/fonts/roboto-slab/roboto-slab.css',
+    'public/static/library/bootstrap-material-design/dist/css/bootstrap-material-design.min.css',
+    'public/static/css/wemd-color-scheme.css',
+    'public/static/css/main.css',
+    'public/static/library/animate.css/animate.min.css',
+    'public/static/fonts/mdi-wxss/MDI.css',
+    'public/static/fonts/devicon/devicon.min.css',
+    'public/static/fonts/langicon/langicon.css',
+    'public/static/fonts/socialicon/socialicon.css',
+], 'public/static/css/noj.css').options({
+    processCssUrls: false
+});
+
+mix.copyDirectory('node_modules', 'public/static/library');
