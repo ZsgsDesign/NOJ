25895fbf6d9c72de9ae6cfa578a115659097cda6
zsgsdesign <zsgsdesign@gmail.com>
2021-08-21 18:02:09 +0800

    add notranslate class for Google Chrome Translator

--- resources/views/js/common/mathjax.blade.php
+++ resources/views/js/common/mathjax.blade.php
@@ -9,3 +9,8 @@
     });
 </script>
 <script type="text/javascript" src="/static/library/mathjax/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
+<script>
+    MathJax.Hub.Register.StartupHook("End",function () {
+        for (const jax of MathJax.Hub.getAllJax()) {jax.SourceElement().previousSibling.classList.add('notranslate')}
+    });
+</script>
