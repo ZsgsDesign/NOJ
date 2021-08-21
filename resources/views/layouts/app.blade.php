39b6986b47736751904033db317d122fe34eae3a
zsgsdesign <zsgsdesign@gmail.com>
2021-08-21 11:03:40 +0800

    use mix for css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -159,17 +159,7 @@
         </div>
     </loading>
     <!-- Style -->
-    <link rel="stylesheet" href="/static/fonts/roboto/roboto.css">
-    <link rel="stylesheet" href="/static/fonts/montserrat/montserrat.css">
-    <link rel="stylesheet" href="/static/fonts/roboto-slab/roboto-slab.css">
-    <link rel="stylesheet" href="/static/library/bootstrap-material-design/dist/css/bootstrap-material-design.min.css">
-    <link rel="stylesheet" href="/static/css/wemd-color-scheme.css">
-    <link rel="stylesheet" href="/static/css/main.css?version={{version()}}">
-    <link rel="stylesheet" href="/static/library/animate.css/animate.min.css">
-    <link rel="stylesheet" href="/static/fonts/mdi-wxss/MDI.css">
-    <link rel="stylesheet" href="/static/fonts/devicon/devicon.min.css?version=1.0.3">
-    <link rel="stylesheet" href="/static/fonts/langicon/langicon.css?version=1.0.2">
-    <link rel="stylesheet" href="/static/fonts/socialicon/socialicon.css?version=1.0.1">
+    @include('layouts.css')
     <!-- Background -->
     <div class="mundb-background-container">
         <img src="">
