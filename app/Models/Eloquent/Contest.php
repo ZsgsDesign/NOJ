5003af463368a16c25d8ed9860c1425ec0b544bb
zsgsdesign <zsgsdesign@gmail.com>
2021-08-23 14:15:34 +0800

    fixed reverse-rank bug

--- app/Models/Eloquent/Contest.php
+++ app/Models/Eloquent/Contest.php
@@ -132,7 +132,7 @@ class Contest extends Model
 
     public function problems()
     {
-        return $this->hasMany('App\Models\Eloquent\ContestProblem', 'cid', 'cid');
+        return $this->hasMany('App\Models\Eloquent\ContestProblem', 'cid', 'cid')->orderBy('index');
     }
 
     public function submissions()
