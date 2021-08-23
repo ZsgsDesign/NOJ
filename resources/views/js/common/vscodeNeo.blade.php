4407bb5c21555dd1a758cbde8b67fbee8a77e90d
zsgsdesign <zsgsdesign@gmail.com>
2021-08-23 19:45:35 +0800

    fixed typo

--- resources/views/js/common/vscodeNeo.blade.php
+++ resources/views/js/common/vscodeNeo.blade.php
@@ -14,6 +14,7 @@
     var editor = null;
     var editorProvider = null;
     var monaco = editorInstance.monaco;
+
     window.addEventListener("load",function() {
         {{ $slot }}
     }, false);
