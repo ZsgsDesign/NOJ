690011990bad643b2a5cf47b33baf79ff6765037
zsgsdesign <zsgsdesign@gmail.com>
2021-08-23 17:16:16 +0800

    override bootstrap interference over monaco editor

--- resources/views/js/common/vscodeNeo.blade.php
+++ resources/views/js/common/vscodeNeo.blade.php
@@ -1,3 +1,13 @@
+<style>
+    .monaco-editor label {
+        font-size: inherit;
+        line-height: inherit;
+        margin-bottom: inherit;
+    }
+    .monaco-list .monaco-list-row {
+        transition: .2s ease-out .0s;
+    }
+</style>
 <script src="{{mix('/static/js/build/noj-editor.js')}}"></script>
 <script>
     var editorInstance = new NOJEditor();
