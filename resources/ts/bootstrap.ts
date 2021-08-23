dfb3887e1b9506eeb7c1a88a945792e11555d44b
zsgsdesign <zsgsdesign@gmail.com>
2021-08-24 06:27:23 +0800

    add jquery and hljs require

--- resources/ts/bootstrap.ts
+++ resources/ts/bootstrap.ts
@@ -1,5 +1,9 @@
 window._ = require('lodash');
 
+window.$ = window.jQuery = require('jquery');
+
+window.hljs = require('highlight.js');
+
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
--- resources/ts/types.d.ts
+++ resources/ts/types.d.ts
@@ -1,4 +1,7 @@
 declare interface Window{
     _: any;
+    hljs: any;
+    $: any;
+    jQuery: any;
     axios: any;
 }
