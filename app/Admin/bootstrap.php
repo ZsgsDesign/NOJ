c83b1400d9ba46efd51633eb54853e65bd9f1402
zsgsdesign <zsgsdesign@gmail.com>
2021-08-24 08:13:00 +0800

    remove hljs css file usage

--- app/Admin/bootstrap.php
+++ app/Admin/bootstrap.php
@@ -22,7 +22,6 @@ Admin::css(mix('/static/css/build/noj_admin.css'));
 Admin::css('/static/fonts/mdi-wxss/MDI.css');
 Admin::js(mix('/static/js/build/noj_admin.js'));
 Admin::favicon('/favicon.png');
-Admin::css('/static/library/highlight.js/styles/vs.css');
 Encore\Admin\Form::forget(['map', 'editor']);
 app('view')->prependNamespace('admin', resource_path('views/admin'));
 
--- resources/views/js/common/hljsLight.blade.php
+++ resources/views/js/common/hljsLight.blade.php
@@ -1,6 +1,72 @@
-<link rel="stylesheet" href="/static/library/highlight.js/styles/vs.css">
+<!-- VS Theme for hljs -->
 <style>
+    pre code.hljs {
+        display: block;
+        overflow-x: auto;
+        padding: 1em
+    }
+
+    code.hljs {
+        padding: 3px 5px
+    }
+
     .hljs {
         background: #fafafa;
+        color: #000
+    }
+
+    .hljs-comment,
+    .hljs-quote,
+    .hljs-variable {
+        color: green
+    }
+
+    .hljs-built_in,
+    .hljs-keyword,
+    .hljs-name,
+    .hljs-selector-tag,
+    .hljs-tag {
+        color: #00f
+    }
+
+    .hljs-addition,
+    .hljs-attribute,
+    .hljs-literal,
+    .hljs-section,
+    .hljs-string,
+    .hljs-template-tag,
+    .hljs-template-variable,
+    .hljs-title,
+    .hljs-type {
+        color: #a31515
+    }
+
+    .hljs-deletion,
+    .hljs-meta,
+    .hljs-selector-attr,
+    .hljs-selector-pseudo {
+        color: #2b91af
+    }
+
+    .hljs-doctag {
+        color: grey
+    }
+
+    .hljs-attr {
+        color: red
+    }
+
+    .hljs-bullet,
+    .hljs-link,
+    .hljs-symbol {
+        color: #00b0e8
+    }
+
+    .hljs-emphasis {
+        font-style: italic
+    }
+
+    .hljs-strong {
+        font-weight: 700
     }
 </style>
