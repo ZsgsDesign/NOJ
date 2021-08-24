b9b464ba6a364be488edb20424a0f6dcaaf0bcb2
zsgsdesign <zsgsdesign@gmail.com>
2021-08-24 22:06:56 +0800

    update paginate method

--- app/Admin/Controllers/ApiController.php
+++ app/Admin/Controllers/ApiController.php
@@ -4,12 +4,30 @@ namespace App\Admin\Controllers;
 
 use App\Models\Eloquent\Problem;
 use Encore\Admin\Controllers\AdminController;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class ApiController extends AdminController
 {
+    protected function paginate($items, $perPage=15, $pageStart=1)
+    {
+        $offSet = ($pageStart * $perPage) - $perPage;
+        $itemsForCurrentPage = array_slice($items, $offSet, $perPage, true);
+        $paginator = new LengthAwarePaginator($itemsForCurrentPage, count($items), $perPage, Paginator::resolveCurrentPage(), ['path' => Paginator::resolveCurrentPath()]);
+        return $paginator;
+    }
+
     public function problems()
     {
         $q = request()->q;
-        return Problem::where('pcode', 'like', "%$q%")->paginate(null, ['pid as id', 'pcode as text']);
+
+        return $this->paginate(Problem::orderBy('pcode', 'asc')->get()->filter(function($problem) use ($q) {
+            return stripos($problem->readable_name, $q) !== false;
+        })->values()->transform(function ($problem) {
+            return [
+                'id' => $problem->pid,
+                'text' => $problem->readable_name,
+            ];
+        })->toArray());
     }
 }
