6e33732e9cb3e284bda8c863ac3020a15869de1c
zsgsdesign <zsgsdesign@gmail.com>
2021-08-25 19:01:35 +0800

    fixed typo

--- app/Models/Eloquent/Dojo/DojoPass.php
+++ app/Models/Eloquent/Dojo/DojoPass.php
@@ -26,7 +26,7 @@ class DojoPass extends Model
 
     public static function isPassed($dojo_id)
     {
-        return Auth::check() ?self::where([
+        return Auth::check() ? self::where([
             "dojo_id"=>$dojo_id,
             "user_id"=>Auth::user()->id,
         ])->count()>0 : false;
