6511381e576fe17de6b5fc607e77f24534ec803d
zsgsdesign <zsgsdesign@gmail.com>
2021-08-26 18:38:57 +0800

    add font Poppin to admin portal

--- app/Admin/bootstrap.php
+++ app/Admin/bootstrap.php
@@ -1,5 +1,7 @@
 <?php
 
+use Encore\Admin\Admin;
+
 /**
  * Laravel-admin - admin builder based on Laravel.
  * @author z-song <https://github.com/z-song>
@@ -20,6 +22,8 @@
 
 Admin::css(mix('/static/css/build/noj_admin.css'));
 Admin::css('/static/fonts/mdi-wxss/MDI.css?version=1.0.1');
+Admin::css('/static/fonts/poppins/poppins.css?version=1.0.0');
+Admin::style(".main-sidebar, .main-footer, .main-header .logo .logo-lg, h1, h2, h3, h4, h5, h6, .h1, .h2, .h3, .h4, .h5, .h6 {font-family:'Poppins';}");
 Admin::js(mix('/static/js/build/noj_admin.js'));
 Admin::favicon('/favicon.png');
 Encore\Admin\Form::forget(['map', 'editor']);
