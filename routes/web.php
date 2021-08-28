20db86004be2b197f778653a29cb94489b08f95e
zsgsdesign <zsgsdesign@gmail.com>
2021-08-29 07:41:21 +0800

    add pdf view for print

--- routes/web.php
+++ routes/web.php
@@ -122,6 +122,7 @@ Route::group([
         Route::get('/{cid}/board/admin/scrollBoard', 'AdminController@scrollBoard')->middleware('auth', 'contest_account', 'privileged')->name('admin.scrollboard');
         Route::get('/{cid}/board/admin/downloadContestAccountXlsx', 'AdminController@downloadContestAccountXlsx')->middleware('auth')->name('admin.download.contestaccountxlsx');
         Route::get('/{cid}/board/admin/refreshContestRank', 'AdminController@refreshContestRank')->middleware('auth')->name('admin.refresh.contestrank');
+        Route::get('/{cid}/board/admin/pdf', 'AdminController@pdf')->middleware('auth')->name('admin.pdf');
     });
 });
 
