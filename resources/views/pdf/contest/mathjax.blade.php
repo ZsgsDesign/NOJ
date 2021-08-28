9d669337acd3245562821ea7febb80f2e3d4fd71
zsgsdesign <zsgsdesign@gmail.com>
2021-08-29 05:45:34 +0800

    add mathjax config for PDF generation

---
+++ resources/views/pdf/contest/mathjax.blade.php
@@ -0,0 +1,17 @@
+<script type="text/x-mathjax-config">
+    MathJax.Hub.Config({
+        "HTML-CSS": {
+            scale: 200
+        },
+        tex2jax: {
+            inlineMath: [ ['$$$','$$$'], ["\\(","\\)"] ],
+            displayMath: [ ["$$$$$$","$$$$$$"], ['$$','$$'], ['\\[', '\\]'] ],
+            processEscapes: true
+        },
+        showMathMenu: false
+    });
+</script>
+<script type="text/javascript" src="{{asset("/static/library/mathjax/MathJax.js?config=TeX-AMS-MML_HTMLorMML")}}"></script>
+<script>
+    MathJax.Hub.Queue(["Rerender", MathJax.Hub], function () {window.status="finished"});
+</script>
