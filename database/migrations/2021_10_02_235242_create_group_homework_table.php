1c16c18b9e32aee1cb23af9675b2956bdf35ff5b
zsgsdesign <zsgsdesign@gmail.com>
2021-10-06 03:45:47 +0800

    use homework to replace homeworks

--- database/migrations/2021_10_02_235242_create_group_homework_table.php
+++ database/migrations/2021_10_02_235242_create_group_homework_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateGroupHomeworksTable extends Migration
+class CreateGroupHomeworkTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateGroupHomeworksTable extends Migration
      */
     public function up()
     {
-        Schema::create('group_homeworks', function (Blueprint $table) {
+        Schema::create('group_homework', function (Blueprint $table) {
             $table->integer('id', true);
             $table->integer('group_id')->nullable();
             $table->foreign('group_id')->references('gid')->on('group')->onUpdate('CASCADE')->onDelete('CASCADE');
@@ -32,6 +32,6 @@ class CreateGroupHomeworksTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('group_homeworks');
+        Schema::dropIfExists('group_homework');
     }
 }
