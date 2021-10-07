321c758d70b9d58e25f70636a84b74e0e56b92e7
zsgsdesign <zsgsdesign@gmail.com>
2021-10-07 20:10:27 +0800

    add orderby for homework list

--- app/Http/Controllers/Group/IndexController.php
+++ app/Http/Controllers/Group/IndexController.php
@@ -161,7 +161,7 @@ class IndexController extends Controller
             'site_title'=>config("app.name"),
             'navigation'=>"Group",
             'group_info'=>$basic_info,
-            'homework_list'=>Group::find($basic_info["gid"])->homework
+            'homework_list'=>Group::find($basic_info["gid"])->homework()->orderBy('created_at', 'desc')->orderBy('id', 'desc')->get()
         ]);
     }
 
