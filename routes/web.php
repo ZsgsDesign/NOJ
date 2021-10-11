4df1614553a55b1e41f86a6ce50a70c57db789c7
ZsgsDesign <zsgsdesign@gmail.com>
2021-10-11 11:16:54 +0800

    use dot separated router for problem index

--- routes/web.php
+++ routes/web.php
@@ -61,7 +61,7 @@ Route::group(['prefix' => 'user','as' => 'user.', 'middleware' => ['user.banned'
 });
 
 Route::group(['prefix' => 'problem', 'middleware' => ['user.banned', 'contest_account']], function () {
-    Route::get('/', 'ProblemController@index')->name('problem_index');
+    Route::get('/', 'ProblemController@index')->name('problem.index');
     Route::get('/{pcode}', 'ProblemController@detail')->name('problem.detail');
     Route::get('/{pcode}/editor', 'ProblemController@editor')->middleware('auth')->name('problem.editor');
     Route::get('/{pcode}/solution', 'ProblemController@solution')->middleware('auth')->name('problem.solution');
