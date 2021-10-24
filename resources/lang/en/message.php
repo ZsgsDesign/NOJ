afe4df4d1db206211de42acfd264f627806afeae
zsgsdesign <zsgsdesign@gmail.com>
2021-10-25 01:04:38 +0800

    add locale for solution and group messages

--- resources/lang/en/message.php
+++ resources/lang/en/message.php
@@ -6,4 +6,29 @@ return [
     'eraseRead'     => 'Erase read',
     'empty'         => 'You have no message.',
     'official'      => ':name Official',
+    'delimiter'     => ', ',
+    'solution' => [
+        'accepted' => [
+            'title' => 'Some of your solutions have been accepted',
+            'desc' => "Your submitted solutions to the following problems have been accepted: \n\n :problemList",
+        ],
+        'declined' => [
+            'title' => 'Some of your solutions have been declined',
+            'desc' => "Your submitted solutions to the following problems have been declined: \n\n :problemList",
+        ],
+    ],
+    'group' => [
+        'agreed' => [
+            'title'   => 'Some users agreed to join your group :name',
+            'desc' => "The following users agreed to join your group :groupInfo :\n\n :userList\n\nYou can manage group members in the group detail page."
+        ],
+        'applied'=> [
+            'title'   => 'Some users applied to join your group :name',
+            'desc' => "The following users applied to join your group :groupInfo :\n\n :userList\n\nYou can manage group members in the group detail page."
+        ],
+        'invited' => [
+            'title'   => ':sender_name invites you to group :group_name',
+            'desc' => "Hi, Dear **:reciver_name**,\n\n  **:sender_name** has just invited you to join the group **[:group_name](:group_url)**. Take a look and meet other fascinating people right now!\n\nSincerely, NOJ"
+        ]
+    ],
 ];
--- resources/lang/zh-CN/message.php
+++ resources/lang/zh-CN/message.php
@@ -6,4 +6,29 @@ return [
     'eraseRead'     => '删除已读',
     'empty'         => '空落落的，什么也没有。',
     'official'      => ':name官方',
+    'delimiter'     => '，',
+    'solution' => [
+        'accepted' => [
+            'title' => '您提交的题解已经通过了',
+            'desc' => "您为如下题目提交的题解已经通过了：\n\n :problemList",
+        ],
+        'declined' => [
+            'title' => '您提交的题解被拒绝了',
+            'desc' => "您为如下题目提交的题解被拒绝了：\n\n :problemList",
+        ],
+    ],
+    'group' => [
+        'agreed' => [
+            'title'   => '他们已同意加入您的群组 :name',
+            'desc' => "如下用户已经同意加入您的群组 :groupInfo ：\n\n :userList\n\n您可在群组页管理群成员。"
+        ],
+        'applied' => [
+            'title'   => '有人申请加入您的群组 :name',
+            'desc' => "如下用户申请加入您的群组 :groupInfo ：\n\n :userList\n\n您可在群组页管理群成员。"
+        ],
+        'invited' => [
+            'title'   => ':sender_name 邀请您加入 :group_name',
+            'desc' => "嗨,  **:reciver_name**,\n\n  **:sender_name** 刚刚邀请您加入 **[:group_name](:group_url)** 群组. 现在就看一看，认识一些有趣的人吧!\n\n NOJ"
+        ]
+    ],
 ];
