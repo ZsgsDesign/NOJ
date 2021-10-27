db33ce4e8ace55d771022935ca398e8a8d274fcc
ZsgsDesign <zsgsdesign@gmail.com>
2021-10-27 23:06:05 +0800

    add homework message locale

--- resources/lang/en/message.php
+++ resources/lang/en/message.php
@@ -10,25 +10,31 @@ return [
     'solution' => [
         'accepted' => [
             'title' => 'Some of your solutions have been accepted',
-            'desc' => "Your submitted solutions to the following problems have been accepted: \n\n :problemList",
+            'desc'  => "Your submitted solutions to the following problems have been accepted: \n\n :problemList",
         ],
         'declined' => [
             'title' => 'Some of your solutions have been declined',
-            'desc' => "Your submitted solutions to the following problems have been declined: \n\n :problemList",
+            'desc'  => "Your submitted solutions to the following problems have been declined: \n\n :problemList",
         ],
     ],
     'group' => [
         'agreed' => [
-            'title'   => 'Some users agreed to join your group :name',
-            'desc' => "The following users agreed to join your group :groupInfo :\n\n :userList\n\nYou can manage group members in the group detail page."
+            'title' => 'Some users agreed to join your group :name',
+            'desc'  => "The following users agreed to join your group :groupInfo :\n\n :userList\n\nYou can manage group members in the group detail page."
         ],
         'applied'=> [
-            'title'   => 'Some users applied to join your group :name',
-            'desc' => "The following users applied to join your group :groupInfo :\n\n :userList\n\nYou can manage group members in the group detail page."
+            'title' => 'Some users applied to join your group :name',
+            'desc'  => "The following users applied to join your group :groupInfo :\n\n :userList\n\nYou can manage group members in the group detail page."
         ],
         'invited' => [
-            'title'   => ':sender_name invites you to group :group_name',
-            'desc' => "Hi, Dear **:reciver_name**,\n\n  **:sender_name** has just invited you to join the group **[:group_name](:group_url)**. Take a look and meet other fascinating people right now!\n\nSincerely, NOJ"
+            'title' => ':sender_name invites you to group :group_name',
+            'desc'  => "Hi, Dear **:reciver_name**,\n\n  **:sender_name** has just invited you to join the group **[:group_name](:group_url)**. Take a look and meet other fascinating people right now!\n\nSincerely, NOJ"
+        ]
+    ],
+    'homework' => [
+        'new' => [
+            'title' => 'You\'ve got one or more new assignments',
+            'desc'  => "The following assignment(s) have been released by admin or manager of groups you participate, please take your time and have a look:\n\n :homeworkList\n\nYou can complete them so long as time don't pass the deadline."
         ]
     ],
 ];
--- resources/lang/zh-CN/message.php
+++ resources/lang/zh-CN/message.php
@@ -10,25 +10,31 @@ return [
     'solution' => [
         'accepted' => [
             'title' => '您提交的题解已经通过了',
-            'desc' => "您为如下题目提交的题解已经通过了：\n\n :problemList",
+            'desc'  => "您为如下题目提交的题解已经通过了：\n\n :problemList",
         ],
         'declined' => [
             'title' => '您提交的题解被拒绝了',
-            'desc' => "您为如下题目提交的题解被拒绝了：\n\n :problemList",
+            'desc'  => "您为如下题目提交的题解被拒绝了：\n\n :problemList",
         ],
     ],
     'group' => [
         'agreed' => [
-            'title'   => '他们已同意加入您的群组 :name',
-            'desc' => "如下用户已经同意加入您的群组 :groupInfo ：\n\n :userList\n\n您可在群组页管理群成员。"
+            'title' => '他们已同意加入您的群组 :name',
+            'desc'  => "如下用户已经同意加入您的群组 :groupInfo ：\n\n :userList\n\n您可在群组页管理群成员。"
         ],
         'applied' => [
-            'title'   => '有人申请加入您的群组 :name',
-            'desc' => "如下用户申请加入您的群组 :groupInfo ：\n\n :userList\n\n您可在群组页管理群成员。"
+            'title' => '有人申请加入您的群组 :name',
+            'desc'  => "如下用户申请加入您的群组 :groupInfo ：\n\n :userList\n\n您可在群组页管理群成员。"
         ],
         'invited' => [
-            'title'   => ':sender_name 邀请您加入 :group_name',
-            'desc' => "嗨,  **:reciver_name**,\n\n  **:sender_name** 刚刚邀请您加入 **[:group_name](:group_url)** 群组. 现在就看一看，认识一些有趣的人吧!\n\n NOJ"
+            'title' => ':sender_name 邀请您加入 :group_name',
+            'desc'  => "嗨,  **:reciver_name**,\n\n  **:sender_name** 刚刚邀请您加入 **[:group_name](:group_url)** 群组. 现在就看一看，认识一些有趣的人吧!\n\n NOJ"
+        ]
+    ],
+    'homework' => [
+        'new' => [
+            'title' => '您有一项或多项作业',
+            'desc'  => "以下是您所参加的群组中由管理员或群主发布的任务，请您查阅：\n\n :homeworkList\n\n您可以在任何时间完成它们，只要没有越过截止日期。"
         ]
     ],
 ];
