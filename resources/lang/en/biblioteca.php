310ce9146670890eef444e6aeb5bc0867ec19b6e
ZsgsDesign <zsgsdesign@gmail.com>
2022-03-08 08:43:06 +0800

    add locale string support for biblioteca

---
+++ resources/lang/en/biblioteca.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'zh' => 'Chinese',
+    'zh-cn' => 'Chinese (Simplified)',
+    'zh-tw' => 'Chinese (Traditional)',
+    'en' => 'English',
+    'en-us' => 'English (United States)',
+    'en-gb' => 'English (Great Britain)',
+    'tlh' => 'Klingon',
+];
