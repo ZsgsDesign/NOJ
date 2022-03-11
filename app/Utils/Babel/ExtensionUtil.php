6f29a3435e3d259d9f80cd89ff06e20b7133d808
ZsgsDesign <zsgsdesign@gmail.com>
2022-03-11 09:19:00 +0800

    format all utils

--- app/Utils/Babel/ExtensionUtil.php
+++ app/Utils/Babel/ExtensionUtil.php
@@ -9,122 +9,122 @@ use Throwable;
 class ExtensionUtil
 {
 
-    public static $status=[
-        "-1"=>"Extension Fatal Error",
-        "0"=>"Not Downloaded",
-        "1"=>"Not Installed",
-        "2"=>"Installed",
+    public static $status = [
+        "-1" => "Extension Fatal Error",
+        "0" => "Not Downloaded",
+        "1" => "Not Installed",
+        "2" => "Installed",
     ];
 
     public static function localList()
     {
-        $ret=[];
-        $marketspaceRaw=self::getRemote();
-        $marketspace=[];
+        $ret = [];
+        $marketspaceRaw = self::getRemote();
+        $marketspace = [];
         foreach ($marketspaceRaw["packages"] as $extension) {
-            $marketspace[$extension["name"]]=$extension;
+            $marketspace[$extension["name"]] = $extension;
         }
 
-        $localList=self::getLocal();
+        $localList = self::getLocal();
 
         foreach ($localList as $extension) {
-            $temp=[
-                "details"=>$extension,
-                "status"=>0,
-                "version"=>null,
-                "updatable"=>false,
-                "settings"=>null,
-                "available"=>null
+            $temp = [
+                "details" => $extension,
+                "status" => 0,
+                "version" => null,
+                "updatable" => false,
+                "settings" => null,
+                "available" => null
             ];
-            $temp["details"]["typeParsed"]=$temp["details"]["type"]=="virtual-judge" ? "VirtualJudge" : "OnlineJudge";
+            $temp["details"]["typeParsed"] = $temp["details"]["type"] == "virtual-judge" ? "VirtualJudge" : "OnlineJudge";
             try {
-                if ($extension["version"]=='__cur__') {
-                    $extension["version"]=explode("-", version())[0];
+                if ($extension["version"] == '__cur__') {
+                    $extension["version"] = explode("-", version())[0];
                 }
-                $downloadedVersion=new Version($extension["version"]);
+                $downloadedVersion = new Version($extension["version"]);
 
                 if (isset($marketspace[$extension["name"]])) {
                     //remote extension, else is local extension
-                    $remoteVersion=new Version($marketspace[$extension["name"]]["version"]);
-                    $temp["updatable"]=$remoteVersion->isGreaterThan($downloadedVersion);
-                    $temp["details"]["official"]=$marketspace[$extension["name"]]["official"];
+                    $remoteVersion = new Version($marketspace[$extension["name"]]["version"]);
+                    $temp["updatable"] = $remoteVersion->isGreaterThan($downloadedVersion);
+                    $temp["details"]["official"] = $marketspace[$extension["name"]]["official"];
                 } else {
-                    $temp["updatable"]=false;
-                    $temp["details"]["official"]=0;
+                    $temp["updatable"] = false;
+                    $temp["details"]["official"] = 0;
                 }
 
-                $installedConfig=OJ::where(["ocode"=>$extension["code"]])->first();
+                $installedConfig = OJ::where(["ocode" => $extension["code"]])->first();
                 if (is_null($installedConfig)) {
-                    $temp["status"]=1;
+                    $temp["status"] = 1;
                 } else {
-                    $temp["version"]=$installedConfig->version; // local installed version
-                    $installedVersion=new Version($temp["version"]);
+                    $temp["version"] = $installedConfig->version; // local installed version
+                    $installedVersion = new Version($temp["version"]);
                     if ($downloadedVersion->isGreaterThan($installedVersion)) {
-                        $temp["status"]=1;
+                        $temp["status"] = 1;
                     } else {
-                        $temp["status"]=2;
+                        $temp["status"] = 2;
                     }
-                    $temp["settings"]=false;
-                    $temp["available"]=$installedConfig->status;
+                    $temp["settings"] = false;
+                    $temp["available"] = $installedConfig->status;
                 }
             } catch (Throwable $e) {
                 continue;
             }
-            $ret[]=$temp;
+            $ret[] = $temp;
         }
         return $ret;
     }
 
     public static function list()
     {
-        $ret=[];
-        $marketspaceRaw=self::getRemote();
+        $ret = [];
+        $marketspaceRaw = self::getRemote();
         if (empty($marketspaceRaw)) {
             return [];
         }
         foreach ($marketspaceRaw["packages"] as $extension) {
-            $temp=[
-                "details"=>$extension,
-                "status"=>0,
-                "version"=>null,
-                "updatable"=>false,
-                "settings"=>null,
-                "available"=>null
+            $temp = [
+                "details" => $extension,
+                "status" => 0,
+                "version" => null,
+                "updatable" => false,
+                "settings" => null,
+                "available" => null
             ];
-            $temp["details"]["typeParsed"]=$temp["details"]["type"]=="virtual-judge" ? "VirtualJudge" : "OnlineJudge";
+            $temp["details"]["typeParsed"] = $temp["details"]["type"] == "virtual-judge" ? "VirtualJudge" : "OnlineJudge";
             try {
                 try {
-                    $BabelConfig=json_decode(file_get_contents(babel_path("Extension/{$extension['code']}/babel.json")), true);
+                    $BabelConfig = json_decode(file_get_contents(babel_path("Extension/{$extension['code']}/babel.json")), true);
                 } catch (Throwable $e) {
-                    $BabelConfig=[];
+                    $BabelConfig = [];
                 }
                 if (!empty($BabelConfig)) {
-                    if ($BabelConfig["version"]=='__cur__') {
-                        $BabelConfig["version"]=explode("-", version())[0];
+                    if ($BabelConfig["version"] == '__cur__') {
+                        $BabelConfig["version"] = explode("-", version())[0];
                     }
-                    $downloadedVersion=new Version($BabelConfig["version"]);
-                    $remoteVersion=new Version($extension["version"]);
-                    $temp["updatable"]=$remoteVersion->isGreaterThan($downloadedVersion);
+                    $downloadedVersion = new Version($BabelConfig["version"]);
+                    $remoteVersion = new Version($extension["version"]);
+                    $temp["updatable"] = $remoteVersion->isGreaterThan($downloadedVersion);
 
-                    $installedConfig=OJ::where(["ocode"=>$extension["code"]])->first();
+                    $installedConfig = OJ::where(["ocode" => $extension["code"]])->first();
                     if (is_null($installedConfig)) {
-                        $temp["status"]=1;
+                        $temp["status"] = 1;
                     } else {
-                        $temp["version"]=$installedConfig->version; // local installed version
-                        $installedVersion=new Version($temp["version"]);
+                        $temp["version"] = $installedConfig->version; // local installed version
+                        $installedVersion = new Version($temp["version"]);
                         if ($downloadedVersion->isGreaterThan($installedVersion)) {
-                            $temp["status"]=1;
+                            $temp["status"] = 1;
                         } else {
-                            $temp["status"]=2;
+                            $temp["status"] = 2;
                         }
-                        $temp["settings"]=false;
-                        $temp["available"]=$installedConfig->status;
+                        $temp["settings"] = false;
+                        $temp["available"] = $installedConfig->status;
                     }
                 }
             } catch (Throwable $e) {
                 continue;
             }
-            $ret[]=$temp;
+            $ret[] = $temp;
         }
 
         return $ret;
@@ -132,13 +132,13 @@ class ExtensionUtil
 
     public static function getLocal()
     {
-        $ret=[];
-        $dirs=array_filter(glob(babel_path("Extension/*")), 'is_dir');
+        $ret = [];
+        $dirs = array_filter(glob(babel_path("Extension/*")), 'is_dir');
         foreach ($dirs as $d) {
-            $extension=basename($d);
-            $BabelConfig=json_decode(file_get_contents(babel_path("Extension/$extension/babel.json")), true);
-            if ($extension==$BabelConfig["code"]) {
-                $ret[]=$BabelConfig;
+            $extension = basename($d);
+            $BabelConfig = json_decode(file_get_contents(babel_path("Extension/$extension/babel.json")), true);
+            if ($extension == $BabelConfig["code"]) {
+                $ret[] = $BabelConfig;
             }
         }
         return $ret;
@@ -147,7 +147,7 @@ class ExtensionUtil
     public static function getRemote()
     {
         try {
-            return json_decode(file_get_contents(config('babel.mirror')."/babel.json"), true);
+            return json_decode(file_get_contents(config('babel.mirror') . "/babel.json"), true);
         } catch (Throwable $e) {
             return [];
         }
@@ -155,13 +155,13 @@ class ExtensionUtil
 
     public static function remoteDetail($code)
     {
-        $babelConfig=self::getRemote();
+        $babelConfig = self::getRemote();
         if (empty($babelConfig)) {
             return [];
         }
-        $babelConfigPackages=$babelConfig["packages"];
+        $babelConfigPackages = $babelConfig["packages"];
         foreach ($babelConfigPackages as $package) {
-            if ($package["code"]==$code) {
+            if ($package["code"] == $code) {
                 return $package;
             }
         }
